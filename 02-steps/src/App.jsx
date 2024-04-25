f9c8c5de0e57e2fa6d170508eb1d4be39b92e51b
Luis Medina <I202215560@cibertec.edu.pe>
2024-04-25 15:26:11 -0500

    feat: Start step project

---
+++ 02-steps/src/App.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const H1 = styled.h1`
+  font-size: 5rem;
+  color: red;
+`;
+
+function App() {
+  return <H1>Hello World!</H1>;
+}
+
+export default App;
