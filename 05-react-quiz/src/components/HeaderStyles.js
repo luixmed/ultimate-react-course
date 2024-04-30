4d0a7b09d46f438d029f6cbf2d89c2c45c6464da
Luis Medina <I202215560@cibertec.edu.pe>
2024-04-29 20:05:13 -0500

    feat: Add Header component

---
+++ 05-react-quiz/src/components/HeaderStyles.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const HeaderStyled = styled.header`
+  /* Box */
+  width: 66rem;
+  margin-bottom: 4rem;
+
+  /* Layout */
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  img {
+    width: 14rem;
+  }
+
+  h1 {
+    font-family: "Codystar";
+    font-size: 5.6rem;
+  }
+`;
