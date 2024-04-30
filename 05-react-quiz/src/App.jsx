5de47d87eb06e561f8f5b29aedb81be69d2b947e
Luis Medina <I202215560@cibertec.edu.pe>
2024-04-29 20:07:40 -0500

    feat: Fetch questions from the server API using useReducer

--- 05-react-quiz/src/App.jsx
+++ 05-react-quiz/src/App.jsx
@@ -1,11 +1,53 @@
+import { useEffect, useReducer } from "react";
 import { AppStyled } from "./AppStyles";
 import { GlobalStyles } from "./GlobalStyles";
+import Header from "./components/Header";
+
+const initialState = {
+  questions: [],
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return {
+        ...state,
+        questions: action.payload,
+        status: "ready",
+      };
+    case "dataFailed":
+      return {
+        ...state,
+        status: "error",
+      };
+  }
+}
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  // eslint-disable-next-line no-unused-vars
+  const { questions, status } = state;
+
+  useEffect(function () {
+    async function fetchQuestions() {
+      try {
+        const res = await fetch("http://localhost:8000/questions");
+        const data = await res.json();
+        dispatch({ type: "dataReceived", payload: data });
+      } catch (err) {
+        dispatch({ type: "dataFailed" });
+      }
+    }
+    fetchQuestions();
+  }, []);
   return (
     <>
       <GlobalStyles />
-      <AppStyled></AppStyled>
+      <AppStyled>
+        <Header />
+      </AppStyled>
     </>
   );
 }
