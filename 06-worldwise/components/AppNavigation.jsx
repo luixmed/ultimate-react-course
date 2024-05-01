b1f2722752d8ad092176540c99a24d5907f191d4
Luis Medina <I202215560@cibertec.edu.pe>
2024-05-01 16:41:56 -0500

    feat: Add App Layout component and App navigation component

---
+++ 06-worldwise/components/AppNavigation.jsx
@@ -0,0 +1,5 @@
+function AppNavigation() {
+  return <div></div>;
+}
+
+export default AppNavigation;
---
+++ 06-worldwise/pages/AppLayout.jsx
@@ -0,0 +1,11 @@
+import AppNavigation from "../components/AppNavigation";
+
+function AppLayout() {
+  return (
+    <div>
+      <AppNavigation />
+    </div>
+  );
+}
+
+export default AppLayout;
