67f39c58092f66a23fc650a3a80e3ee3200b4789
Luis Medina <I202215560@cibertec.edu.pe>
2024-05-01 18:26:14 -0500

    feat: Add styling for Product component

--- 06-worldwise/pages/Product.jsx
+++ 06-worldwise/pages/Product.jsx
@@ -1,10 +1,31 @@
 import PageNavigation from "../components/PageNavigation";
+import { PricingStyled } from "./PricingStyles";
 
 function Product() {
   return (
-    <div>
+    <PricingStyled>
       <PageNavigation />
-    </div>
+
+      <section>
+        <img
+          src="img-1.jpg"
+          alt="person with dog overlooking mountain with sunset"
+        />
+        <div>
+          <h2>About WorldWide</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum,
+            laboriosam. Harum distinctio placeat quidem voluptatum impedit illum
+            iste eligendi fugiat minus!
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore,
+            perspiciatis. Totam, minus. Harum dolorem repellat expedita quam,
+            debitis corporis id eos.
+          </p>
+        </div>
+      </section>
+    </PricingStyled>
   );
 }
 
