e0b5622698e240e6322d42aed37e594b8f0e0b0d
Luis Medina <I202215560@cibertec.edu.pe>
2024-05-01 16:40:44 -0500

    feat: Add global styles

--- 06-worldwise/src/GlobalStyles.js
+++ 06-worldwise/src/GlobalStyles.js
@@ -1,6 +1,18 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyles = createGlobalStyle`
+  :root {
+    --color-brand--1: #ffb545;
+    --color-brand--2: #00c46a;
+
+    --color-dark--0: #242a2e;
+    --color-dark--1: #2d3439;
+    --color-dark--2: #42484d;
+    --color-light--1: #aaa;
+    --color-light--2: #ececec;
+    --color-light--3: #d6dee0;
+  }
+
   * {
     margin: 0;
     padding: 0;
@@ -8,11 +20,19 @@ export const GlobalStyles = createGlobalStyle`
   }
 
   html {
+
     box-sizing: border-box;
-    font-size: 62.5%
+    font-size: 62.5%;
+  
   }
 
   body {
+    /* Font */
+    font-family: "Manrope", sans-serif;
     font-size: 1.6rem;
+    font-weight: 400;
+    color: var(--color-light--2);
+    line-height: 1.6;
+
   }
 `;
