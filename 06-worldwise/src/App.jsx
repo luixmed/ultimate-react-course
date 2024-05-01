6352a102055104f2939ee1fa54acc202ce4d362a
Luis Medina <I202215560@cibertec.edu.pe>
2024-05-01 15:58:27 -0500

    feat: Add routes for the principal project pages

--- 06-worldwise/src/App.jsx
+++ 06-worldwise/src/App.jsx
@@ -1,5 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from "../pages/HomePage";
+import Pricing from "../pages/Pricing";
+import Product from "../pages/Product";
+import Login from "../pages/Login";
+
 function App() {
-  return <div>WORLDWISE</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="product" element={<Product />} />
+        <Route path="login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
