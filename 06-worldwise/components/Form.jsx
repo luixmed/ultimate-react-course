6cd8d4c7c7fdccf07eaff84a58317f034c00cdea
Luis Medina <I202215560@cibertec.edu.pe>
2024-05-03 14:12:33 -0500

    feat: Implement geolocation-based city data retrieval
    This feature enables us to fetch city data using geolocation coordinates. By leveraging this information, we can accurately set the state for the selected city on the map.

--- 06-worldwise/components/Form.jsx
+++ 06-worldwise/components/Form.jsx
@@ -1,13 +1,58 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { FormStyled } from "./FormStyles";
 import Button from "./Button";
 import BackButton from "./BackButton";
+import { useUrlPosition } from "../hooks/useUrlPosition";
+import Spinner from "./Spinner";
+import Message from "./Message";
+
+const BASE_URL = "https://api.bigdatacloud.net/data/reverse-geocode-client";
 
 function Form() {
   const [cityName, setCityName] = useState("");
   // eslint-disable-next-line no-unused-vars
   const [emoji, setEmoji] = useState("");
   const [notes, setNotes] = useState("");
+  const [isLoadingGeocoding, setIsLoadingGeocoding] = useState(false);
+  // eslint-disable-next-line no-unused-vars
+  const [country, setCountry] = useState("");
+  const [geocodingError, setGeocodingError] = useState("");
+
+  const [lat, lng] = useUrlPosition();
+
+  useEffect(
+    function () {
+      async function fetchCityData() {
+        try {
+          setIsLoadingGeocoding(true);
+          setGeocodingError("");
+          const res = await fetch(
+            `${BASE_URL}?latitude=${lat}&longitude=${lng}`
+          );
+          const data = await res.json();
+
+          if (!data.countryCode)
+            throw new Error(
+              "That doesn't seem to be a city. Click somewhere else 😉"
+            );
+
+          setCityName(data.city || data.locality || "");
+          setCountry(data.countryName);
+          setEmoji(data.countryCode);
+        } catch (err) {
+          setGeocodingError(err.message);
+        } finally {
+          setIsLoadingGeocoding(false);
+        }
+      }
+      fetchCityData();
+    },
+    [lat, lng]
+  );
+
+  if (isLoadingGeocoding) return <Spinner />;
+
+  if (geocodingError) return <Message message={geocodingError} />;
 
   return (
     <FormStyled>
