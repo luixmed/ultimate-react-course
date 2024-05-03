20d703dca511b2d8e9622dd4c4a8879f7149fb38
Luis Medina <I202215560@cibertec.edu.pe>
2024-05-03 11:13:53 -0500

    styles: Add basic styling for the Form component

--- 06-worldwise/components/FormStyles.js
+++ 06-worldwise/components/FormStyles.js
@@ -1,3 +1,38 @@
 import styled from "styled-components";
 
-export const FormStyled = styled.form``;
+export const FormStyled = styled.form`
+  /* Layout */
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+
+  /* Box */
+  width: 100%;
+  padding: 2rem 3rem;
+  border-radius: 7px;
+
+  /* Style */
+  background-color: var(--color-dark--2);
+
+  div:not(:last-child) {
+    display: flex;
+    flex-direction: column;
+    gap: 0.5rem;
+
+    position: relative;
+
+    span {
+      font-size: 2.8rem;
+
+      /* Layout */
+      position: absolute;
+      top: 2.7rem;
+      right: 1rem;
+    }
+  }
+
+  div:last-child {
+    display: flex;
+    justify-content: space-between;
+  }
+`;
