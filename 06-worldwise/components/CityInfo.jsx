5e16e9ed24811e80f10d7c7fdb1053f25bff432f
Luis Medina <I202215560@cibertec.edu.pe>
2024-05-03 06:43:44 -0500

    feat: Add CityInfo component
    This component uses the cities custom context
    provider for retireving city data based on its
    unique id.

--- 06-worldwise/components/CityInfo.jsx
+++ 06-worldwise/components/CityInfo.jsx
@@ -1,10 +1,59 @@
 import { useParams } from "react-router-dom";
 import { CityInfoStyled } from "./CityInfoStyles";
+import { useEffect } from "react";
+import { useCities } from "../contexts/CitiesContext";
+import Spinner from "./Spinner";
 
 function CityInfo() {
   const { cityId } = useParams();
+  const { currentCity, getCity, isLoading } = useCities();
 
-  return <CityInfoStyled>City ID: {cityId}</CityInfoStyled>;
+  useEffect(
+    function () {
+      getCity(cityId);
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [cityId]
+  );
+
+  // eslint-disable-next-line no-unused-vars
+  const { cityName, emoji, date, notes } = currentCity;
+
+  if (isLoading) return <Spinner />;
+
+  return (
+    <CityInfoStyled>
+      <div>
+        <h6>City Name</h6>
+        <h3>
+          <span>{emoji}</span> {cityName}
+        </h3>
+      </div>
+
+      <div>
+        <h6>You went to {cityName} on</h6>
+        <p>{date || null}</p>
+      </div>
+
+      {notes && (
+        <div>
+          <h6>Your notes</h6>
+          <p>{notes}</p>
+        </div>
+      )}
+
+      <div>
+        <h6>Learn more</h6>
+        <a
+          href={`https://en.wikipedia.org/wiki/${cityName}`}
+          target="_blank"
+          rel="noreferrer"
+        >
+          Check out {cityName} on Wikipedia &rarr;
+        </a>
+      </div>
+    </CityInfoStyled>
+  );
 }
 
 export default CityInfo;
--- 06-worldwise/components/CityInfoStyles.js
+++ 06-worldwise/components/CityInfoStyles.js
@@ -1,3 +1,54 @@
 import styled from "styled-components";
 
-export const CityInfoStyled = styled.div``;
+export const CityInfoStyled = styled.div`
+  /* Box */
+  width: 100%;
+  max-height: 70%;
+  padding: 2rem 3rem;
+  border-radius: 7px;
+
+  /* Style */
+  background-color: var(--color-dark--2);
+  overflow: hidden;
+
+  /* Layout */
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+
+  div:not(:last-child) {
+    display: flex;
+    flex-direction: column;
+    gap: 0.5rem;
+  }
+
+  h6 {
+    color: var(--color-light--1);
+    font-size: 1.1rem;
+    font-weight: 900;
+    text-transform: uppercase;
+  }
+
+  h3 {
+    font-size: 1.9rem;
+
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+
+    span {
+      font-size: 3.2rem;
+      line-height: 1;
+    }
+  }
+
+  p {
+    font-size: 1.6rem;
+  }
+
+  a:link,
+  a:visited {
+    color: var(--color-brand--1);
+    font-size: 1.6rem;
+  }
+`;
