4f8bba97e8ff476fdc957bea5c6f3972651bb09f
Luis Medina <I202215560@cibertec.edu.pe>
2024-05-03 11:25:20 -0500

    feat: Add a custom geolocation hook

---
+++ 06-worldwise/hooks/useGeoLocation.js
@@ -0,0 +1,30 @@
+import { useState } from "react";
+
+export function useGeoLocation(defaultPosition = null) {
+  const [position, setPosition] = useState(defaultPosition);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  function getPosition() {
+    if (!navigator.geolocation)
+      return setError("Your browser does not support feolocation");
+
+    setIsLoading(true);
+
+    navigator.geolocation.getCurrentPosition(
+      (pos) => {
+        setPosition({
+          lat: pos.coords.latitude,
+          lng: pos.coords.longitude,
+        });
+        setIsLoading(false);
+      },
+      (error) => {
+        setError(error.message);
+        setIsLoading(false);
+      }
+    );
+  }
+
+  return { position, isLoading, error, getPosition };
+}
