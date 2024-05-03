36b11b030a796b86b644055b7480182913fe9316
Luis Medina <I202215560@cibertec.edu.pe>
2024-05-03 09:51:24 -0500

    feat: Add markers for each city in the CityList component
    This commit adds markers to display the city's name and its country flag.

--- 06-worldwise/components/Map.jsx
+++ 06-worldwise/components/Map.jsx
@@ -2,10 +2,12 @@ import { useSearchParams } from "react-router-dom";
 import { MapStyled } from "./MapStyles";
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import { useState } from "react";
+import { useCities } from "../contexts/CitiesContext";
 
 function Map() {
   // eslint-disable-next-line no-unused-vars
   const [mapPosition, setMapPosition] = useState([51.505, -0.09]);
+  const { cities } = useCities();
 
   // eslint-disable-next-line no-unused-vars
   const [searchParams, setSearchParams] = useSearchParams();
@@ -18,18 +20,24 @@ function Map() {
       <MapContainer
         center={mapPosition}
         zoom={13}
-        scrollWheelZoom={false}
+        scrollWheelZoom={true}
         className="map"
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={mapPosition}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
+        {cities.map((city) => (
+          <Marker
+            position={[city.position.lat, city.position.lng]}
+            key={city.id}
+          >
+            <Popup>
+              <span>{city.emoji}</span>
+              <span>{city.cityName}</span>
+            </Popup>
+          </Marker>
+        ))}
       </MapContainer>
     </MapStyled>
   );
