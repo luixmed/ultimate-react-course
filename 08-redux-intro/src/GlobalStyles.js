c36f7339db77456608fa91317a6b09bc1ac46020
Luis Medina <I202215560@cibertec.edu.pe>
2024-05-04 09:48:48 -0500

    style: Add global styling for the app

--- 08-redux-intro/src/GlobalStyles.js
+++ 08-redux-intro/src/GlobalStyles.js
@@ -10,6 +10,32 @@ export const GlobalStyles = createGlobalStyle`
     font-size: 62.5%;
   }
   body {
+    /* Font */
     font-size: 1.6rem;
+    font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
+    Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+
+    /* Box */
+    margin: 40px;
+  }
+
+  input,
+  select {
+    /* Font */
+    font: inherit;
+    /* Box */
+    padding: 4px 8px;
+    margin: 0 8px;
+  }
+
+  button {
+    cursor: pointer;
+    
+    /* Font */
+    font-weight: bold;
+    text-transform: uppercase;
+
+    /* Box */
+    padding: 6px 8px;
   }
 `;
