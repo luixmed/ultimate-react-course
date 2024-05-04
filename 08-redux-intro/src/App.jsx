1bb7c7dba9d64bb0741e5ffceb3f795ef76b24b2
Luis Medina <I202215560@cibertec.edu.pe>
2024-05-04 10:07:09 -0500

    feat: Add better logic for displaying the App component

--- 08-redux-intro/src/App.jsx
+++ 08-redux-intro/src/App.jsx
@@ -1,16 +1,24 @@
+import { useSelector } from "react-redux";
 import CreateCustomer from "../features/customers/CreateCustomer";
 import Customer from "../features/customers/Customer";
 import { AppStyled } from "./AppStyles";
 import { GlobalStyles } from "./GlobalStyles";
 
 function App() {
+  const fullName = useSelector((state) => state.customer.fullName);
+
   return (
     <>
       <GlobalStyles />
       <AppStyled>
         <h1>🏦 The React-Redux Bank </h1>
-        <CreateCustomer />
-        <Customer />
+        {!fullName ? (
+          <CreateCustomer />
+        ) : (
+          <>
+            <Customer />
+          </>
+        )}
       </AppStyled>
     </>
   );
