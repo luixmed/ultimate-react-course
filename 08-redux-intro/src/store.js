ece58459ed0d148710e3bb98692aa03a77a98f25
Luis Medina <I202215560@cibertec.edu.pe>
2024-05-04 17:39:31 -0500

    feat: Add the new store created with the new @reduxjs/toolkit

--- 08-redux-intro/src/store.js
+++ 08-redux-intro/src/store.js
@@ -1,13 +1,11 @@
-import { applyMiddleware, combineReducers, createStore } from "redux";
-import { thunk } from "redux-thunk";
+import { configureStore } from "@reduxjs/toolkit";
+
 import accountReducer from "../features/accounts/accountSlice";
 import customerReducer from "../features/customers/customerSlice";
 
-const rootReducer = combineReducers({
+const store = configureStore({
   account: accountReducer,
   customer: customerReducer,
 });
-
-const store = createStore(rootReducer, applyMiddleware(thunk));
 
 export default store;
