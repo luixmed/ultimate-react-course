db65c66ccf6c6bf1fdf30946502f5a6f3ac1ad53
Luis Medina <I202215560@cibertec.edu.pe>
2024-05-04 11:18:55 -0500

    style: Refactor global styling

--- 08-redux-intro/src/GlobalStyles.js
+++ 08-redux-intro/src/GlobalStyles.js
@@ -1,17 +1,8 @@
 import { createGlobalStyle } from "styled-components";
 export const GlobalStyles = createGlobalStyle`
-  * {
-    margin: 0;
-    padding: 0;
-    box-sizing: inherit;
-  }
-  html {
-    box-sizing: border-box;
-    font-size: 62.5%;
-  }
+  
   body {
     /* Font */
-    font-size: 1.6rem;
     font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
     Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
 
@@ -30,7 +21,7 @@ export const GlobalStyles = createGlobalStyle`
 
   button {
     cursor: pointer;
-    
+
     /* Font */
     font-weight: bold;
     text-transform: uppercase;
