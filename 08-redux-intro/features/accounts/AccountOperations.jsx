845336deeede2ba2369d4d46e9b426098ada962f
Luis Medina <I202215560@cibertec.edu.pe>
2024-05-04 17:25:32 -0500

    bug: Correct initial currency value in handleDeposit function
    In this commit, we've addressed a bug in the `handleDeposit` function within the AccountOperations component. The initial currency value is now correctly set to "USD," ensuring accurate handling of deposit transactions.

--- 08-redux-intro/features/accounts/AccountOperations.jsx
+++ 08-redux-intro/features/accounts/AccountOperations.jsx
@@ -24,7 +24,7 @@ function AccountOperations() {
     if (!depositAmount) return;
     dispatch(deposit(depositAmount, currency));
     setDepositAmount("");
-    setCurrency("");
+    setCurrency("USD");
   }
 
   function handleWithdrawal() {
