c9a36e36aa0e9cfaafb3ecc2ebce74b083588c95
Luis Medina <I202215560@cibertec.edu.pe>
2024-05-04 06:50:39 -0500

    feat: Create the store

--- 08-redux-intro/src/store.js
+++ 08-redux-intro/src/store.js
@@ -1,3 +1,5 @@
+import { createStore } from "redux";
+
 const initialState = {
   balance: 0,
   loan: 0,
@@ -27,3 +29,11 @@ function reducer(state = initialState, action) {
       return state;
   }
 }
+
+const store = createStore(reducer);
+
+store.dispatch({ type: "account/deposit", payload: 500 });
+console.log(store.getState());
+
+store.dispatch({ type: "account/withdraw", payload: 200 });
+console.log(store.getState());
