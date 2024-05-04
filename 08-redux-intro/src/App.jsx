28199878032787a05c14d4911e27ca5054567856
Luis Medina <I202215560@cibertec.edu.pe>
2024-05-04 06:14:47 -0500

    feat: Set up project initial configuration

---
+++ 08-redux-intro/src/App.jsx
@@ -0,0 +1,13 @@
+import { AppStyled } from "./AppStyles";
+import { GlobalStyles } from "./GlobalStyles";
+
+function App() {
+  return (
+    <>
+      <GlobalStyles />
+      <AppStyled>HELLO</AppStyled>
+    </>
+  );
+}
+
+export default App;
---
+++ 08-redux-intro/src/AppStyles.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const AppStyled = styled.div`
+  color: white;
+  background-color: #333;
+  min-height: 100vh;
+`;
