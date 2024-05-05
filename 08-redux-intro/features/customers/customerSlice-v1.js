f395170d795d4daa8a37ee1c527e0f3ad24d24df
Luis Medina <I202215560@cibertec.edu.pe>
2024-05-04 19:46:13 -0500

    Save the old redux implementation of the customerReducer

---
+++ 08-redux-intro/features/customers/customerSlice-v1.js
@@ -0,0 +1,39 @@
+const initialStateCustomer = {
+  fullName: "",
+  nationalID: "",
+  createdAt: "",
+};
+
+export default function customerReducer(state = initialStateCustomer, action) {
+  switch (action.type) {
+    case "customer/createCustomer":
+      return {
+        ...state,
+        fullName: action.payload.fullName,
+        nationalID: action.payload.nationalID,
+        createdAt: action.payload.createdAt,
+      };
+    case "customer/updateFullName":
+      return {
+        ...state,
+        fullName: action.payload,
+      };
+    default:
+      return state;
+  }
+}
+
+export function createCustomer(fullName, nationalID) {
+  return {
+    type: "customer/createCustomer",
+    payload: {
+      fullName,
+      nationalID,
+      createdAt: new Date().toISOString(),
+    },
+  };
+}
+
+export function updateFullName(fullName) {
+  return { type: "customer/updateFullName", payload: fullName };
+}
