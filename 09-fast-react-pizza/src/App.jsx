31d97400f78efb5ea5d7daee09aba9e1f921b340
Luis Medina <I202215560@cibertec.edu.pe>
2024-05-06 17:14:35 -0500

    feat: Integrate menu data loader with Menu component route in App
    In this commit, we've linked the menu data loader function to the route for the Menu component within the App component. Now, when users navigate to the menu section, the loader ensures seamless retrieval of menu data.

--- 09-fast-react-pizza/src/App.jsx
+++ 09-fast-react-pizza/src/App.jsx
@@ -2,7 +2,7 @@
 import { GlobalStyles } from "./GlobalStyles";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "../ui/Home";
-import Menu from "../features/menu/Menu";
+import Menu, { loader as menuLoader } from "../features/menu/Menu";
 import Cart from "../features/cart/Cart";
 import CreateOrder from "../features/orders/CreateOrder";
 import Order from "../features/orders/Order";
@@ -13,7 +13,7 @@ const router = createBrowserRouter([
     element: <AppLayout />,
     children: [
       { path: "/", element: <Home /> },
-      { path: "/menu", element: <Menu /> },
+      { path: "/menu", element: <Menu />, loader: menuLoader },
       { path: "/cart", element: <Cart /> },
       { path: "/order/new", element: <CreateOrder /> },
       { path: "/order/:orderId", element: <Order /> },
