80266fb71e6f2b9bca5d31c2840dc9464de996ab
Luis Medina <I202215560@cibertec.edu.pe>
2024-05-06 18:09:16 -0500

    feat: Add the Loader component to the AppLayout componentto show the user when the menu data is loading on that component

--- 09-fast-react-pizza/ui/AppLayout.jsx
+++ 09-fast-react-pizza/ui/AppLayout.jsx
@@ -1,11 +1,17 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import CartOverview from "../features/cart/CartOverview";
 import { AppLayoutStyled } from "./AppLayoutStyles";
 import Header from "./Header";
+import Loader from "./Loader";
 
 function AppLayout() {
+  const navigation = useNavigation();
+  const isLoading = navigation.state === "loading";
+
   return (
     <AppLayoutStyled>
+      {isLoading && <Loader />}
+
       <Header />
 
       <main>
