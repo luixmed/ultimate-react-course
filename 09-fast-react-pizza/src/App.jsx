2d527e5fb8b02f3be734c8a7c95dc0de85caa4bd
Luis Medina <I202215560@cibertec.edu.pe>
2024-05-06 15:33:22 -0500

    feat: Add the route for the AppLayout component

--- 09-fast-react-pizza/src/App.jsx
+++ 09-fast-react-pizza/src/App.jsx
@@ -6,13 +6,19 @@ import Menu from "../features/menu/Menu";
 import Cart from "../features/cart/Cart";
 import CreateOrder from "../features/orders/CreateOrder";
 import Order from "../features/orders/Order";
+import AppLayout from "../ui/AppLayout";
 
 const router = createBrowserRouter([
-  { path: "/", element: <Home /> },
-  { path: "/menu", element: <Menu /> },
-  { path: "/cart", element: <Cart /> },
-  { path: "/order/new", element: <CreateOrder /> },
-  { path: "/order/:orderId", element: <Order /> },
+  {
+    element: <AppLayout />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/menu", element: <Menu /> },
+      { path: "/cart", element: <Cart /> },
+      { path: "/order/new", element: <CreateOrder /> },
+      { path: "/order/:orderId", element: <Order /> },
+    ],
+  },
 ]);
 
 function App() {
