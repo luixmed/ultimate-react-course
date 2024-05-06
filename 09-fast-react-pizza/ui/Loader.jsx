67c7bbd6d65fd33a6a3ab7fe443a0bb759554b64
Luis Medina <I202215560@cibertec.edu.pe>
2024-05-06 18:05:07 -0500

    style: Add styling for the Loader component

--- 09-fast-react-pizza/ui/Loader.jsx
+++ 09-fast-react-pizza/ui/Loader.jsx
@@ -1,6 +1,34 @@
 import styled from "styled-components";
 
-const LoaderStyled = styled.div``;
+const LoaderStyled = styled.div`
+  /* Box */
+  width: 45px;
+  aspect-ratio: 0.75;
+
+  --c: no-repeat linear-gradient(brown 0 0);
+
+  /* Styles */
+  background: var(--c) 0% 50%, var(--c) 50% 50%, var(--c) 100% 50%;
+  background-size: 20% 50%;
+
+  /* Animation */
+  animation: loading 1s infinite linear;
+
+  @keyframes loading {
+    20% {
+      background-position: 0% 0%, 50% 50%, 100% 50%;
+    }
+    40% {
+      background-position: 0% 100%, 50% 0%, 100% 50%;
+    }
+    60% {
+      background-position: 0% 50%, 50% 100%, 100% 0%;
+    }
+    80% {
+      background-position: 0% 50%, 50% 50%, 100% 100%;
+    }
+  }
+`;
 
 function Loader() {
   return <LoaderStyled></LoaderStyled>;
