37245b23298d0b2bf3448156cf1d3ba0bc9f7fd3
Luis Medina <I202215560@cibertec.edu.pe>
2024-05-06 12:58:41 -0500

    deat: Set up intial project configuration

---
+++ 09-fast-react-pizza/src/App.jsx
@@ -0,0 +1,13 @@
+import { AppStyled } from "./AppStyles";
+import { GlobalStyles } from "./GlobalStyles";
+
+function App() {
+  return (
+    <>
+      <GlobalStyles />
+      <AppStyled>Hello VITE!</AppStyled>
+    </>
+  );
+}
+
+export default App;
