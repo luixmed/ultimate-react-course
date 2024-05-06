13749f07d74637f7493474555b5db2dfe038c22f
Luis Medina <I202215560@cibertec.edu.pe>
2024-05-06 18:29:39 -0500

    feat: Handle errors with react-router-dom
    Set the errorElement in the App component to render the Error component for corresponding routes.

--- 09-fast-react-pizza/src/App.jsx
+++ 09-fast-react-pizza/src/App.jsx
@@ -7,13 +7,20 @@ import Cart from "../features/cart/Cart";
 import CreateOrder from "../features/orders/CreateOrder";
 import Order from "../features/orders/Order";
 import AppLayout from "../ui/AppLayout";
+import Error from "../ui/Error";
 
 const router = createBrowserRouter([
   {
     element: <AppLayout />,
+    errorElement: <Error />,
     children: [
       { path: "/", element: <Home /> },
-      { path: "/menu", element: <Menu />, loader: menuLoader },
+      {
+        path: "/menu",
+        element: <Menu />,
+        loader: menuLoader,
+        errorElement: <Error />,
+      },
       { path: "/cart", element: <Cart /> },
       { path: "/order/new", element: <CreateOrder /> },
       { path: "/order/:orderId", element: <Order /> },
