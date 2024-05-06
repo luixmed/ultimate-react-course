a41afbcead178c6412528b0c4056fcbfb6cb1a8a
Luis Medina <I202215560@cibertec.edu.pe>
2024-05-06 15:26:50 -0500

    feat: Set up overall app structure with AppLayout component
    In this commit, we've established the general structure for our entire app using the `AppLayout` component. The layout includes the following key elements:
    - Header (for navigation)
    - Outlet (main content area)
    - CartOverview (for managing cart-related information)
    
    This foundational setup ensures consistent organization and facilitates seamless navigation throughout the app.

--- 09-fast-react-pizza/ui/AppLayout.jsx
+++ 09-fast-react-pizza/ui/AppLayout.jsx
@@ -1,7 +1,20 @@
+import { Outlet } from "react-router-dom";
+import CartOverview from "../features/cart/CartOverview";
 import { AppLayoutStyled } from "./AppLayoutStyles";
+import Header from "./Header";
 
 function AppLayout() {
-  return <AppLayoutStyled>AppLayout</AppLayoutStyled>;
+  return (
+    <AppLayoutStyled>
+      <Header />
+
+      <main>
+        <Outlet />
+      </main>
+
+      <CartOverview />
+    </AppLayoutStyled>
+  );
 }
 
 export default AppLayout;
