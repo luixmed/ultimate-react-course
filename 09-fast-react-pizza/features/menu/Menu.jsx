726851b7cdf17301c532d4ca8a396b3e0c98feca
Luis Medina <I202215560@cibertec.edu.pe>
2024-05-06 17:12:27 -0500

    feat: Implement loader function in Menu component for menu data retrieval
    In this commit, we've added a loader function to the Menu component. This function is responsible for fetching menu data using react-router-dom. Now, when users access the menu section, the loader ensures that the data is retrieved seamlessly.

--- 09-fast-react-pizza/features/menu/Menu.jsx
+++ 09-fast-react-pizza/features/menu/Menu.jsx
@@ -1,7 +1,18 @@
+import { useLoaderData } from "react-router-dom";
+import { getMenu } from "../../services/apiRestaurant";
 import { MenuStyled } from "./MenuStyled";
 
 function Menu() {
+  const menu = useLoaderData();
+  console.log(menu);
+
   return <MenuStyled>MENU</MenuStyled>;
+}
+
+// eslint-disable-next-line react-refresh/only-export-components
+export async function loader() {
+  const menu = await getMenu();
+  return menu;
 }
 
 export default Menu;
