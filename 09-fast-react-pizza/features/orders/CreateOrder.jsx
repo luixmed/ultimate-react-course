6874fe90fe270ef02362a5fa8fab257e2af726ee
Luis Medina <I202215560@cibertec.edu.pe>
2024-05-07 10:55:11 -0500

    feat: Implement basic static structure for the CreateOrder component
    This commit introduces the initial static structure for the CreateOrder component. The component comprises a form enabling users to input personal and order-related information to create a new order.

--- 09-fast-react-pizza/features/orders/CreateOrder.jsx
+++ 09-fast-react-pizza/features/orders/CreateOrder.jsx
@@ -1,7 +1,70 @@
+import { Form } from "react-router-dom";
 import { CreateOrderStyled } from "./CreateOrderStyled";
 
+const fakeCart = [
+  {
+    pizzaId: 12,
+    name: "Mediterranean",
+    quantity: 2,
+    unitPrice: 16,
+    totalPrice: 32,
+  },
+  {
+    pizzaId: 6,
+    name: "Vegetale",
+    quantity: 1,
+    unitPrice: 13,
+    totalPrice: 13,
+  },
+  {
+    pizzaId: 11,
+    name: "Spinach and Mushroom",
+    quantity: 1,
+    unitPrice: 15,
+    totalPrice: 15,
+  },
+];
+
 function CreateOrder() {
-  return <CreateOrderStyled>CREATE-ORDER</CreateOrderStyled>;
+  const cart = fakeCart;
+
+  return (
+    <CreateOrderStyled>
+      <h2>Ready to order? Let&apos;s go!</h2>
+
+      <Form method="POST">
+        {/* USER'S NAME */}
+        <div>
+          <label>First name</label>
+          <input type="text" name="customer" required />
+        </div>
+
+        {/* PHONE */}
+        <div>
+          <label>Phone number</label>
+          <input type="tel" name="phone" required />
+        </div>
+
+        {/* ADDRESS */}
+        <div>
+          <label>Address</label>
+          <input type="text" name="address" required />
+        </div>
+
+        {/* ORDER PRIORITY */}
+        <div>
+          <input type="checkbox" name="priority" id="priority" />
+          <label htmlFor="priority">Want to give your order priority?</label>
+        </div>
+
+        {/* CART and SUBMIT BUTTON */}
+        <div>
+          <input type="hidden" name="cart" value={JSON.stringify(cart)} />
+          <button>Order now</button>
+        </div>
+      </Form>
+    </CreateOrderStyled>
+  );
 }
 
 export default CreateOrder;
