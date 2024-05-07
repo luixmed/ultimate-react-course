29cc1e8cc2fa7c05892b9bba67cc3dc4cb1cd77d
Luis Medina <I202215560@cibertec.edu.pe>
2024-05-07 14:18:36 -0500

    style: Implement styling for the SearchOrder component

--- 09-fast-react-pizza/features/orders/SearchOrderStyles.js
+++ 09-fast-react-pizza/features/orders/SearchOrderStyles.js
@@ -1,3 +1,13 @@
 import styled from "styled-components";
 
-export const SearchOrderStyled = styled.form``;
+export const SearchOrderStyled = styled.form`
+  input {
+    width: 25.6rem;
+
+    &:focus {
+      width: 28.8rem;
+      outline: 1px solid #e677004c;
+      outline-offset: 2px;
+    }
+  }
+`;
