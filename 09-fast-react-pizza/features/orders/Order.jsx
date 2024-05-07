92aec3c100f15fcc561bface99d4a624356fd03a
Luis Medina <I202215560@cibertec.edu.pe>
2024-05-06 20:01:15 -0500

    feat: Integrate react-router-dom loader function for searching orders by ID
    This commit implements the loader function from react-router-dom to facilitate the search of orders by their ID from the restaurant API. Additionally, it enhances the Order component to display the retrieved order information.

--- 09-fast-react-pizza/features/orders/Order.jsx
+++ 09-fast-react-pizza/features/orders/Order.jsx
@@ -1,41 +1,12 @@
+import { useLoaderData } from "react-router-dom";
+import { getOrder } from "../../services/apiRestaurant";
 import { OrderStyled } from "./OrderStyles";
 
-const order = {
-  id: "ABCDEF",
-  customer: "Jonas",
-  phone: "123456789",
-  address: "Arroios, Lisbon , Portugal",
-  priority: true,
-  estimatedDelivery: "2027-04-25T10:00:00",
-  cart: [
-    {
-      pizzaId: 7,
-      name: "Napoli",
-      quantity: 3,
-      unitPrice: 16,
-      totalPrice: 48,
-    },
-    {
-      pizzaId: 5,
-      name: "Diavola",
-      quantity: 2,
-      unitPrice: 16,
-      totalPrice: 32,
-    },
-    {
-      pizzaId: 3,
-      name: "Romana",
-      quantity: 1,
-      unitPrice: 15,
-      totalPrice: 15,
-    },
-  ],
-  position: "-9.000,38.000",
-  orderPrice: 95,
-  priorityPrice: 19,
-};
+// Test ID: IIDSAT
 
 function Order() {
+  const order = useLoaderData();
+
   const {
     // id,
     status,
@@ -68,6 +39,12 @@ function Order() {
       </div>
     </OrderStyled>
   );
+}
+
+// eslint-disable-next-line react-refresh/only-export-components
+export async function loader({ params }) {
+  const order = await getOrder(params.orderId);
+  return order;
 }
 
 export default Order;
