52ebb1c47bfbd6ca350786e47bd435c068763f6e
Luis Medina <I202215560@cibertec.edu.pe>
2024-05-06 19:18:17 -0500

    feat: Add the SearchOrder component to the Header component

--- 09-fast-react-pizza/ui/Header.jsx
+++ 09-fast-react-pizza/ui/Header.jsx
@@ -1,10 +1,12 @@
 import { Link } from "react-router-dom";
 import { HeaderStyled } from "./HeaderStyles";
+import SearchOrder from "../features/orders/SearchOrder";
 
 function Header() {
   return (
     <HeaderStyled>
       <Link to="/">Fast React Pizza Co.</Link>
+      <SearchOrder />
       <p>LUIS</p>
     </HeaderStyled>
   );
