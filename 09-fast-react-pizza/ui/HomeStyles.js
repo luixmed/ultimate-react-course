885ea55ad063b6e7914d17c39669e520335e23e6
Luis Medina <I202215560@cibertec.edu.pe>
2024-05-07 15:43:10 -0500

    style: Implement styling for the Home component

--- 09-fast-react-pizza/ui/HomeStyles.js
+++ 09-fast-react-pizza/ui/HomeStyles.js
@@ -1,3 +1,24 @@
 import styled from "styled-components";
 
-export const HomeStyled = styled.div``;
+export const HomeStyled = styled.div`
+  /* Font */
+  text-align: center;
+
+  /* Box */
+  padding-inline: 1.6rem;
+  margin-top: 6.4rem;
+
+  h1 {
+    /* Font */
+    font-size: 3rem;
+    font-weight: 600;
+    line-height: 3.6rem;
+
+    /* Box */
+    margin-bottom: 3.2rem;
+
+    span {
+      color: #eab308;
+    }
+  }
+`;
