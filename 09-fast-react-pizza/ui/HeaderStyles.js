6aa3bc6cf7125f7e7ea8790da4237b40299a8e2a
Luis Medina <I202215560@cibertec.edu.pe>
2024-05-07 14:16:51 -0500

    style: Implement styling for the Header component

--- 09-fast-react-pizza/ui/HeaderStyles.js
+++ 09-fast-react-pizza/ui/HeaderStyles.js
@@ -1,3 +1,22 @@
 import styled from "styled-components";
 
-export const HeaderStyled = styled.header``;
+export const HeaderStyled = styled.header`
+  /* Layout */
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  /* Box */
+  padding: 1.2rem 2.4rem;
+  border-bottom: 1px solid #d6d3d1;
+
+  /* Style */
+  background-color: #facc15;
+
+  a {
+    color: inherit;
+    text-transform: uppercase;
+    letter-spacing: 1.6px;
+    outline: none;
+  }
+`;
