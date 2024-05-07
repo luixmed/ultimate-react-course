866e55b0b51537ea510476bfe03b4bbf5916dfd7
Luis Medina <I202215560@cibertec.edu.pe>
2024-05-07 12:18:51 -0500

    feat: Implement phone number validation within react-router-dom action function
    This commit enhances the react-router-dom action function to include functionality for validating the user's phone number. By adding this feature, the application ensures that only valid phone numbers are accepted during order creation.

--- 09-fast-react-pizza/features/orders/CreateOrder.jsx
+++ 09-fast-react-pizza/features/orders/CreateOrder.jsx
@@ -2,6 +2,11 @@ import { Form, redirect } from "react-router-dom";
 import { CreateOrderStyled } from "./CreateOrderStyled";
 import { createOrder } from "../../services/apiRestaurant";
 
+const isValidPhone = (str) =>
+  /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
+    str
+  );
+
 const fakeCart = [
   {
     pizzaId: 12,
@@ -79,8 +84,15 @@ export async function action({ request }) {
     priority: data.priority === "on",
   };
 
-  const newOrder = await createOrder(order);
+  const errors = {};
+  if (!isValidPhone(order.phone))
+    errors.phoneError =
+      "Please give us your correct phone number. We might need it to contact you.";
 
+  if (Object.keys(errors).length > 0) return errors;
+
+  // If everything is okay, create new order and redirect
+  const newOrder = await createOrder(order);
   return redirect(`/order/${newOrder.id}`);
 }
 
