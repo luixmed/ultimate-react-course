9c44ecc4b133c4312e883db5901f9452e339e1b4
Luis Medina <I202215560@cibertec.edu.pe>
2024-05-06 19:59:01 -0500

    refactor: Fix the return value for the getOrder function of the apiRestaurant.js file

--- 09-fast-react-pizza/services/apiRestaurant.js
+++ 09-fast-react-pizza/services/apiRestaurant.js
@@ -3,7 +3,17 @@ const API_URL = "https://react-fast-pizza-api.onrender.com/api";
 export async function getMenu() {
   const res = await fetch(`${API_URL}/menu`);
   if (!res.ok) throw new Error("Failed getting menu");
+
   const { data } = await res.json();
-  console.log(data);
+  return data;
+}
+
+// Test ID: IIDSAT
+
+export async function getOrder(id) {
+  const res = await fetch(`${API_URL}/order/${id}`);
+  if (!res.ok) throw new Error(`Couldn't find order #${id}`);
+
+  const { data } = await res.json();
   return data;
 }
