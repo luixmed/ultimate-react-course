4866d8bddaa9c300e06a68f8f1753df73e9fa769
Luis Medina <I202215560@cibertec.edu.pe>
2024-05-06 20:04:51 -0500

    feat: Connect the loader function to the route for the Order component
    This commit establishes the connection between the loader function and the route designated for the Order component. By linking these elements, the application now seamlessly loads order data when navigating to the Order component route.

--- 09-fast-react-pizza/src/App.jsx
+++ 09-fast-react-pizza/src/App.jsx
@@ -5,7 +5,7 @@ import Home from "../ui/Home";
 import Menu, { loader as menuLoader } from "../features/menu/Menu";
 import Cart from "../features/cart/Cart";
 import CreateOrder from "../features/orders/CreateOrder";
-import Order from "../features/orders/Order";
+import Order, { loader as orderLoader } from "../features/orders/Order";
 import AppLayout from "../ui/AppLayout";
 import Error from "../ui/Error";
 
@@ -23,7 +23,12 @@ const router = createBrowserRouter([
       },
       { path: "/cart", element: <Cart /> },
       { path: "/order/new", element: <CreateOrder /> },
-      { path: "/order/:orderId", element: <Order /> },
+      {
+        path: "/order/:orderId",
+        element: <Order />,
+        loader: orderLoader,
+        errorElement: <Error />,
+      },
     ],
   },
 ]);
