16601d3cf1915e42cdb2c51afc76563c84bb9e8e
Luis Medina <I202215560@cibertec.edu.pe>
2024-05-07 14:15:16 -0500

    style: Apply general styling to input elements
    This commit introduces general styling enhancements for input elements across the application. By adding consistent styling, the appearance and user experience of input fields are improved.

--- 09-fast-react-pizza/src/GlobalStyles.js
+++ 09-fast-react-pizza/src/GlobalStyles.js
@@ -13,14 +13,41 @@ export const GlobalStyles = createGlobalStyle`
   }
 
   body {
+    /* Font */
+    font-family: "Roboto Mono", monospace;
     font-size: 1.6rem;
+    color: #44403C;
 
     /* ERASE */
-    color: white;
-    background-color: #555;
+    background-color: #f5f5f4;
   }
 
   a {
     text-decoration: none;
+  }
+
+  input {
+    /* Font */
+    font-size: 1.4rem;
+    font-family: inherit;
+
+    /* Box */
+    border: none;
+    padding: 0.8rem 1.6rem;
+    border-radius: 10rem;
+
+
+    /* Style */
+    background-color: #fef9c3;
+
+    transition: all 0.3s;
+
+    &::placeholder {
+      color: rgb(168 162 158);
+    }
+
+    &:focus {
+      outline: none;
+    }
   }
 `;
