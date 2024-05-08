09a4b123253e0982614d6a20376ab0486df5dc49
Luis Medina <I202215560@cibertec.edu.pe>
2024-05-08 10:55:41 -0500

    style: Implement styling for the CreateUser component

---
+++ 09-fast-react-pizza/features/users/CreateUserStyles.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const CreateUserStyled = styled.form`
+  p {
+    color: rgb(87 83 78);
+    font-size: 1.6rem;
+    margin-bottom: 1rem;
+  }
+
+  input {
+    width: 28.8rem;
+    margin-bottom: 3.2rem;
+  }
+`;
