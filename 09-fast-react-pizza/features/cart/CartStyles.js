6ad0214511fa047c8071a4ae5d4bc74e7db88acb
Luis Medina <I202215560@cibertec.edu.pe>
2024-05-07 20:22:50 -0500

    style: Implement styling for the Cart component

--- 09-fast-react-pizza/features/cart/CartStyles.js
+++ 09-fast-react-pizza/features/cart/CartStyles.js
@@ -1,3 +1,20 @@
 import styled from "styled-components";
 
-export const CartStyled = styled.div``;
+export const CartStyled = styled.div`
+  /* Box */
+  padding: 1.2rem 1.6rem;
+
+  h2 {
+    font-size: 2rem;
+    font-weight: 600;
+    margin-top: 2.8rem;
+  }
+
+  div {
+    margin-top: 2.4rem;
+
+    & > a {
+      margin-right: 1rem;
+    }
+  }
+`;
