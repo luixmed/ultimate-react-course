10bda16f0b8d38cefe14477d8cdb6d322c033183
Luis Medina <I202215560@cibertec.edu.pe>
2024-05-08 10:56:24 -0500

    feat: Add the CreateUSer in the Home component

--- 09-fast-react-pizza/ui/Home.jsx
+++ 09-fast-react-pizza/ui/Home.jsx
@@ -1,14 +1,25 @@
+import { useSelector } from "react-redux";
 import Button from "./Button";
 import { HomeStyled } from "./HomeStyles";
+import CreateUser from "../features/users/CreateUser";
 
 function Home() {
+  const username = useSelector((state) => state.user.username);
+
   return (
     <HomeStyled>
       <h1>
-        The best pizza <br />
+        The best pizza. <br />
         <span>Straight out of the oven, straight to you.</span>
       </h1>
-      <Button type="primary">Continue ordering USER_NAME</Button>
+
+      {!username ? (
+        <CreateUser />
+      ) : (
+        <Button type="primary" to="/menu">
+          Continue ordering, {username}
+        </Button>
+      )}
     </HomeStyled>
   );
 }
