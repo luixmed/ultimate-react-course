4487012ebe2503f8e9a537f0d887e172782caf6d
Luis Medina <I202215560@cibertec.edu.pe>
2024-05-07 20:20:25 -0500

    refactor: Change the Link for the LinkButton component

--- 09-fast-react-pizza/features/cart/Cart.jsx
+++ 09-fast-react-pizza/features/cart/Cart.jsx
@@ -1,11 +1,18 @@
+import Button from "../../ui/Button";
+import LinkButton from "../../ui/LinkButton";
 import { CartStyled } from "./CartStyles";
-import { Link } from "react-router-dom";
 
 function Cart() {
   return (
     <CartStyled>
-      <Link to="/menu">&larr; Back to menu</Link>
+      <LinkButton to="/menu">&larr; Back to menu</LinkButton>
       <h2>Your cart, USER_NAME</h2>
+      <div>
+        <Button to="/order/new" type="primary">
+          Order pizzas
+        </Button>
+        <Button type="secondary">Clear cart</Button>
+      </div>
     </CartStyled>
   );
 }
