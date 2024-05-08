83999f25c12abcfe22962b757048d1fc2fa11766
Luis Medina <I202215560@cibertec.edu.pe>
2024-05-08 09:30:35 -0500

    style: Implement styling for the Pizza component

--- 09-fast-react-pizza/features/menu/PizzaStyles.js
+++ 09-fast-react-pizza/features/menu/PizzaStyles.js
@@ -1,3 +1,57 @@
 import styled from "styled-components";
 
-export const PizzaStyled = styled.li``;
+export const PizzaStyled = styled.li`
+  /* Box */
+  border-bottom: 1px solid rgb(231 229 228);
+  padding-block: 0.8rem;
+
+  /* Layout */
+  display: flex;
+  gap: 1.6rem;
+
+  img {
+    height: 9.6rem;
+  }
+
+  & > div {
+    /* Box */
+    padding-top: 2px;
+
+    /* Layout */
+    flex-grow: 1;
+
+    display: flex;
+    flex-direction: column;
+
+    & > p:first-child {
+      font-weight: 500;
+    }
+
+    & > p:nth-child(2) {
+      /* Font */
+      color: rgb(120 113 108);
+      font-size: 1.4rem;
+      font-style: italic;
+      text-transform: uppercase;
+    }
+
+    div {
+      /* Font */
+      font-size: 1.4rem;
+
+      /* Box */
+      margin-top: auto;
+
+      /* Layout */
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+
+      p.sold-out {
+        color: rgb(120 113 108);
+        font-weight: 500;
+        text-transform: uppercase;
+      }
+    }
+  }
+`;
