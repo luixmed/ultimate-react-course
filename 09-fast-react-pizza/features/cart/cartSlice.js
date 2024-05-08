6c248fc064ac1fdb67d45be2fa0ed27bc82a78c0
Luis Medina <I202215560@cibertec.edu.pe>
2024-05-08 15:12:34 -0500

    refactor: Update variable name for pizzaId in reducers
    This commit corrects the variable name used for the item's cart id in the reducers deleteItem, increaseItemQuantity, and decreaseItemQuantity. The incorrect "id" variable has been replaced with the correct "pizzaId," ensuring consistency and clarity in the application's state management.

--- 09-fast-react-pizza/features/cart/cartSlice.js
+++ 09-fast-react-pizza/features/cart/cartSlice.js
@@ -14,17 +14,17 @@ const cartSlice = createSlice({
     },
     deleteItem(state, action) {
       // payload = itemId
-      state.cart = state.cart.filter((item) => item.id !== action.payload);
+      state.cart = state.cart.filter((item) => item.pizzaId !== action.payload);
     },
     increaseItemQuantity(state, action) {
       // payload = itemId
-      const item = state.cart.find((item) => item.id === action.payload);
+      const item = state.cart.find((item) => item.pizzaId === action.payload);
       item.quantity++;
       item.totalPrice = item.quantity * item.unitPrice;
     },
     decreaseItemQuantity(state, action) {
       // payload = itemId
-      const item = state.cart.find((item) => item.id === action.payload);
+      const item = state.cart.find((item) => item.pizzaId === action.payload);
       item.quantity--;
       item.totalPrice = item.quantity * item.unitPrice;
     },
