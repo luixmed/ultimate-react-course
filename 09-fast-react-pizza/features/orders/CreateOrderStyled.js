58bac02a2c31ddc916cce10893afedc8ef9acbe9
Luis Medina <I202215560@cibertec.edu.pe>
2024-05-07 22:20:22 -0500

    style: Implement styling for the CreateOrder component

--- 09-fast-react-pizza/features/orders/CreateOrderStyled.js
+++ 09-fast-react-pizza/features/orders/CreateOrderStyled.js
@@ -1,3 +1,62 @@
 import styled from "styled-components";
 
-export const CreateOrderStyled = styled.div``;
+export const CreateOrderStyled = styled.div`
+  /* Box */
+  padding: 2.4rem 1.6rem;
+
+  h2 {
+    font-size: 2rem;
+    font-weight: 600;
+    margin-bottom: 3.2rem;
+  }
+`;
+
+export const FormStyled = styled.form`
+  & > div {
+    /* Box */
+    margin-bottom: 2rem;
+
+    display: flex;
+    align-items: center;
+
+    label {
+      flex-basis: 16rem;
+    }
+
+    & > input:not([type="checkbox"]),
+    & > div {
+      flex-grow: 1;
+
+      & > input {
+        width: 100%;
+      }
+
+      p {
+        /* Font */
+        color: rgb(185 28 28);
+        font-size: 1.2rem;
+        text-align: center;
+        line-height: 1.6rem;
+
+        /* Box */
+        padding: 8px;
+        border-radius: 6px;
+        margin-top: 0.8rem;
+
+        /* Style */
+        background-color: rgb(254 226 226);
+      }
+    }
+
+    &:has(span) {
+      position: relative;
+
+      span {
+        position: absolute;
+        top: 5px;
+        right: 5px;
+        z-index: 50;
+      }
+    }
+  }
+`;
