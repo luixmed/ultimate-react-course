c340c495a9ed98eeb690e52fabd8e2874eadc450
Luis Medina <I202215560@cibertec.edu.pe>
2024-05-07 20:19:54 -0500

    style: Implement styling for the LinkButton component

---
+++ 09-fast-react-pizza/ui/LinkButtonStyles.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const LinkButtonStyled = styled.a`
+  /* Font */
+  color: rgb(59 130 246);
+  font-size: 1.4rem;
+
+  /* Style */
+  cursor: pointer;
+
+  &:hover {
+    color: rgb(37 99 235);
+    text-decoration-line: underline;
+  }
+`;
