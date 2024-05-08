b2fabe30e783098bbd30fc04505a9a88a5bde497
Luis Medina <I202215560@cibertec.edu.pe>
2024-05-08 18:43:48 -0500

    style: Implement styling for the UpdateItemQuantity component

---
+++ 09-fast-react-pizza/features/cart/UpdateItemQuantityStyles.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const UpdateItemQuantityStyled = styled.div`
+  /* Layout */
+  display: flex;
+  align-items: center;
+  gap: 1.2rem;
+
+  & > span {
+    font-size: 1.3rem;
+    font-weight: 500;
+  }
+`;
