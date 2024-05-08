77409e9b440ed44cf5b124cfc97d11aec01d8cbe
Luis Medina <I202215560@cibertec.edu.pe>
2024-05-07 19:06:48 -0500

    style: Implement styling for the Error component

--- 09-fast-react-pizza/ui/ErrorStyles.js
+++ 09-fast-react-pizza/ui/ErrorStyles.js
@@ -1,3 +1,26 @@
 import styled from "styled-components";
 
-export const ErrorStyled = styled.div``;
+export const ErrorStyled = styled.div`
+  /* Font */
+  text-align: center;
+
+  /* Box */
+  max-width: 80rem;
+  margin-inline: auto;
+  margin-top: 12.8rem;
+  padding-block: 2.4rem;
+  border-radius: 1rem;
+
+  /* Style */
+  background-color: #ffe3e3;
+
+  h1 {
+    margin-bottom: 3.2rem;
+  }
+
+  p {
+    color: #f03e3e;
+    font-size: 2.4rem;
+    font-weight: 700;
+  }
+`;
