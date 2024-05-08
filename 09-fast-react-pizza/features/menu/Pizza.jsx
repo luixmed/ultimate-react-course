d7c6f35f93e0ab01da31085119e3579f90b66787
Luis Medina <I202215560@cibertec.edu.pe>
2024-05-08 11:59:07 -0500

    feat: Add functionality to add a new pizza to cart state
    This commit implements the functionality to add a new pizza item to the cart state. By adding this feature, users can now easily add pizzas to their cart for purchase, enhancing the shopping experience of the application.

--- 09-fast-react-pizza/features/menu/Pizza.jsx
+++ 09-fast-react-pizza/features/menu/Pizza.jsx
@@ -1,8 +1,24 @@
+import { useDispatch } from "react-redux";
 import Button from "../../ui/Button";
 import { PizzaStyled } from "./PizzaStyles";
+import { addItem } from "../cart/cartSlice";
 
 function Pizza({ pizza }) {
-  const { name, unitPrice, imageUrl, ingredients, soldOut } = pizza;
+  const { id, name, unitPrice, imageUrl, ingredients, soldOut } = pizza;
+
+  const dispatch = useDispatch();
+
+  function handleAddToCart() {
+    const newItem = {
+      pizzaId: id,
+      name,
+      quantity: 1,
+      unitPrice,
+      totalPrice: unitPrice * 1,
+    };
+
+    dispatch(addItem(newItem));
+  }
 
   return (
     <PizzaStyled>
@@ -12,7 +28,11 @@ function Pizza({ pizza }) {
         <p>{ingredients.join(", ")}</p>
         <div>
           {!soldOut ? <p>{unitPrice}</p> : <p className="sold-out">Sold out</p>}
-          <Button type="small">Add to cart</Button>
+          {!soldOut && (
+            <Button type="small" onClick={handleAddToCart}>
+              Add to cart
+            </Button>
+          )}
         </div>
       </div>
     </PizzaStyled>
