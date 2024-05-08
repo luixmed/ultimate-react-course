63e802ba67b4e30984cf16eab2546e83ad7a1eca
Luis Medina <I202215560@cibertec.edu.pe>
2024-05-08 10:19:39 -0500

    refactor: Add the Username component

--- 09-fast-react-pizza/ui/Header.jsx
+++ 09-fast-react-pizza/ui/Header.jsx
@@ -1,13 +1,14 @@
 import { Link } from "react-router-dom";
 import { HeaderStyled } from "./HeaderStyles";
 import SearchOrder from "../features/orders/SearchOrder";
+import Username from "../features/users/Username";
 
 function Header() {
   return (
     <HeaderStyled>
       <Link to="/">Fast React Pizza Co.</Link>
       <SearchOrder />
-      <p>LUIS</p>
+      <Username />
     </HeaderStyled>
   );
 }
