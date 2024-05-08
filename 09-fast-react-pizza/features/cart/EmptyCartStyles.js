0f6a15a5e0b244d70f9e3720b24d5e4030504779
Luis Medina <I202215560@cibertec.edu.pe>
2024-05-08 14:51:57 -0500

    style: Implement styling for the EmptyCart component

---
+++ 09-fast-react-pizza/features/cart/EmptyCartStyles.js
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const EmptyCartStyled = styled.div`
+  /* Box */
+  padding: 1.2rem 1.6rem;
+
+  p {
+    /* Font */
+    color: #380f00;
+    font-size: 2.4rem;
+    font-weight: 600;
+    text-align: center;
+
+    /* Box */
+    width: fit-content;
+    margin-inline: auto;
+    padding: 1.6rem 2.4rem;
+    margin-top: 4.8rem;
+    border-radius: 1rem;
+    border: 1px solid currentColor;
+
+    /* Style */
+    background-color: #fff3d9;
+  }
+`;
