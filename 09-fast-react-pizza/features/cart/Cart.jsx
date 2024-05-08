cea1c24a3782bf87826be86139325780f90da406
Luis Medina <I202215560@cibertec.edu.pe>
2024-05-08 14:26:12 -0500

    feat: Implement display of cart items in Cart component
    This commit adds functionality to the Cart component for displaying the items currently in the cart. By integrating this feature, users can easily view and manage the items they have added to their cart, enhancing the usability of the application.

--- 09-fast-react-pizza/features/cart/Cart.jsx
+++ 09-fast-react-pizza/features/cart/Cart.jsx
@@ -3,14 +3,24 @@ import Button from "../../ui/Button";
 import LinkButton from "../../ui/LinkButton";
 import { CartStyled } from "./CartStyles";
 import { getUsername } from "../users/userSlice";
+import { getCart } from "./cartSlice";
+import CartItem from "./CartItem";
 
 function Cart() {
   const username = useSelector(getUsername);
+  const cart = useSelector(getCart);
 
   return (
     <CartStyled>
       <LinkButton to="/menu">&larr; Back to menu</LinkButton>
       <h2>Your cart, {username}</h2>
+
+      <ul>
+        {cart.map((item) => (
+          <CartItem key={item.pizzaId} item={item} />
+        ))}
+      </ul>
+
       <div>
         <Button to="/order/new" type="primary">
           Order pizzas
