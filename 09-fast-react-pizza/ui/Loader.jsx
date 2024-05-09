3849d2769cd60b74202ab3ae4a7239d74bf6eeae
Luis Medina <I202215560@cibertec.edu.pe>
2024-05-09 11:23:48 -0500

    refactor: Fix styling for the Loader component

--- 09-fast-react-pizza/ui/Loader.jsx
+++ 09-fast-react-pizza/ui/Loader.jsx
@@ -1,37 +1,54 @@
 import styled from "styled-components";
 
 const LoaderStyled = styled.div`
-  /* Box */
-  width: 45px;
-  aspect-ratio: 0.75;
+  /* Layout */
+  position: absolute;
+  inset: 0;
 
-  --c: no-repeat linear-gradient(brown 0 0);
+  display: grid;
+  place-content: center;
 
-  /* Styles */
-  background: var(--c) 0% 50%, var(--c) 50% 50%, var(--c) 100% 50%;
-  background-size: 20% 50%;
+  /* Style */
+  background-color: rgb(226 232 240 / 0.2);
+  backdrop-filter: blur(4px);
 
-  /* Animation */
-  animation: loading 1s infinite linear;
+  div.loader {
+    /* Box */
+    width: 45px;
+    aspect-ratio: 0.75;
 
-  @keyframes loading {
-    20% {
-      background-position: 0% 0%, 50% 50%, 100% 50%;
-    }
-    40% {
-      background-position: 0% 100%, 50% 0%, 100% 50%;
-    }
-    60% {
-      background-position: 0% 50%, 50% 100%, 100% 0%;
-    }
-    80% {
-      background-position: 0% 50%, 50% 50%, 100% 100%;
+    --c: no-repeat linear-gradient(#292524 0 0);
+
+    /* Styles */
+    background: var(--c) 0% 50%, var(--c) 50% 50%, var(--c) 100% 50%;
+    background-size: 20% 50%;
+
+    /* Animation */
+    animation: loading 1s infinite linear;
+
+    @keyframes loading {
+      20% {
+        background-position: 0% 0%, 50% 50%, 100% 50%;
+      }
+      40% {
+        background-position: 0% 100%, 50% 0%, 100% 50%;
+      }
+      60% {
+        background-position: 0% 50%, 50% 100%, 100% 0%;
+      }
+      80% {
+        background-position: 0% 50%, 50% 50%, 100% 100%;
+      }
     }
   }
 `;
 
 function Loader() {
-  return <LoaderStyled></LoaderStyled>;
+  return (
+    <LoaderStyled>
+      <div className="loader"></div>
+    </LoaderStyled>
+  );
 }
 
 export default Loader;
