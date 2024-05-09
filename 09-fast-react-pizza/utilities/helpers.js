95ff51a229f13f4ca52847467d81f4f2f2967144
Luis Medina <I202215560@cibertec.edu.pe>
2024-05-09 10:28:24 -0500

    feat: Implement formatDate function for date formatting
    This commit introduces the formatDate function, which converts a provided date into the format "25 Apr, 12:30 PM," for example. This functionality enhances the application by providing a consistent and readable date format across various components and user interfaces.

--- 09-fast-react-pizza/utilities/helpers.js
+++ 09-fast-react-pizza/utilities/helpers.js
@@ -3,3 +3,12 @@ export function calcMinutesLeft(dateStr) {
   const d2 = new Date(dateStr).getTime();
   return Math.round((d2 - d1) / 60000);
 }
+
+export function formatDate(dateStr) {
+  return new Intl.DateTimeFormat("en", {
+    day: "numeric",
+    month: "short",
+    hour: "2-digit",
+    minute: "2-digit",
+  }).format(new Date(dateStr));
+}
