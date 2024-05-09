46a9e1b7c9e6dc341e8f77f5c6c0926533997000
Luis Medina <I202215560@cibertec.edu.pe>
2024-05-09 18:13:07 -0500

    feat: Connect AppLayout to route in App component
    This commit establishes the connection between the AppLayout component and its route in the App component. The majority of pages are now nested as child routes within the AppLayout component, providing a centralized layout structure for the application. This approach enhances organization and facilitates consistent layout management across different pages.

--- 10-the-wild-oasis/src/App.jsx
+++ 10-the-wild-oasis/src/App.jsx
@@ -1,12 +1,13 @@
 import { GlobalStyle } from "./GlobalStyles";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import Bookings from "./pages/Bookings";
 import NewUsers from "./pages/Users";
 import Settings from "./pages/Settings";
 import Account from "./pages/Account";
 import Login from "./pages/Login";
-import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./ui/AppLayout";
+// import PageNotFound from "./pages/PageNotFound";
 
 function App() {
   return (
@@ -14,13 +15,17 @@ function App() {
       <GlobalStyle />
       <BrowserRouter>
         <Routes>
-          <Route path="dashboard" element={<Dashboard />} />
-          <Route path="bookings" element={<Bookings />} />
-          <Route path="users" element={<NewUsers />} />
-          <Route path="settings" element={<Settings />} />
-          <Route path="account" element={<Account />} />
+          <Route element={<AppLayout />}>
+            <Route index element={<Navigate replace to="dashboard" />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="bookings" element={<Bookings />} />
+            <Route path="users" element={<NewUsers />} />
+            <Route path="settings" element={<Settings />} />
+            <Route path="account" element={<Account />} />
+          </Route>
+
           <Route path="login" element={<Login />} />
-          <Route path="*" element={<PageNotFound />} />
+          {/* <Route path="*" element={<PageNotFound />} /> */}
         </Routes>
       </BrowserRouter>
     </>
