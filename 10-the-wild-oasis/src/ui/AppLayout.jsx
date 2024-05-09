6fe6797ce64c1d8fda0bfb14b7d79b216e489e75
Luis Medina <I202215560@cibertec.edu.pe>
2024-05-09 18:49:09 -0500

    feat: Implement general layout for the app
    This commit adds the general layout for the app, positioning the Sidebar, Header, and Main components in their correct positions. By establishing this layout structure, we create a cohesive and organized visual hierarchy for the application, enhancing user navigation and experience.

--- 10-the-wild-oasis/src/ui/AppLayout.jsx
+++ 10-the-wild-oasis/src/ui/AppLayout.jsx
@@ -1,11 +1,32 @@
 import { Outlet } from "react-router-dom";
+import Header from "./Header";
+import Sidebar from "./Sidebar";
+import styled from "styled-components";
+
+const Main = styled.main`
+  background-color: var(--color-grey-50);
+  padding: 4rem 4.8rem 6.4rem;
+`;
+
+const StyledAppLayout = styled.div`
+  /* Box */
+  height: 100vh;
+
+  /* Layout */
+  display: grid;
+  grid-template-columns: 26rem 1fr;
+  grid-template-rows: auto 1fr;
+`;
 
 function AppLayout() {
   return (
-    <>
-      APP LAYOUT
-      <Outlet />
-    </>
+    <StyledAppLayout>
+      <Header />
+      <Sidebar />
+      <Main>
+        <Outlet />
+      </Main>
+    </StyledAppLayout>
   );
 }
 
--- 10-the-wild-oasis/src/ui/Sidebar.jsx
+++ 10-the-wild-oasis/src/ui/Sidebar.jsx
@@ -1,5 +1,17 @@
+import styled from "styled-components";
+
+const StyledSidebar = styled.aside`
+  background-color: var(--color-grey-0);
+
+  /* Box */
+  padding: 3.2rem 2.4rem;
+  border-right: 1px solid var(--color-grey-100);
+
+  /* Layout */
+  grid-row: 1 / -1;
+`;
 function Sidebar() {
-  return <div>SIDEBAR</div>;
+  return <StyledSidebar>SIDEBAR</StyledSidebar>;
 }
 
 export default Sidebar;
