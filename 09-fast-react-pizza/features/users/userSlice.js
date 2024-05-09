7d9bbfcad4894271eceb60cb6cb903cff656a97d
Luis Medina <I202215560@cibertec.edu.pe>
2024-05-08 21:00:03 -0500

    feat: Introduce middleware to fetch customer address by geolocation coordinates
    This commit adds a middleware to fetch the address of customers by their geolocation coordinates. The fetchAddress action creator function is created to handle the retrieval of addresses from a reverse geolocation API. Additionally, new state properties such as status (idle, loading, error), position (latitude and longitude), address, and error are introduced to manage the asynchronous fetching process. The middleware handles pending, fulfilled, and rejected promises states effectively, enhancing the application's geolocation features and user experience.

--- 09-fast-react-pizza/features/users/userSlice.js
+++ 09-fast-react-pizza/features/users/userSlice.js
@@ -1,7 +1,18 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { getAddress } from "../../services/apiGeocoding";
+
+function getGeolocationPosition() {
+  return new Promise(function (resolve, reject) {
+    navigator.geolocation.getCurrentPosition(resolve, reject);
+  });
+}
 
 const initialState = {
   username: "",
+  status: "idle",
+  position: {},
+  address: "",
+  error: "",
 };
 
 const userSlice = createSlice({
@@ -12,7 +23,36 @@ const userSlice = createSlice({
       state.username = action.payload;
     },
   },
+  extraReducers: (builder) =>
+    builder
+      .addCase(fetchAddress.pending, (state) => (state.status = "loading"))
+      .addCase(fetchAddress.fulfilled, (state, action) => {
+        state.position = action.payload.position;
+        state.address = action.payload.address;
+        state.status = "idle";
+      })
+      .addCase(fetchAddress.rejected, (state) => {
+        state.status = "error";
+        state.error =
+          "There was a problem getting your address. Make sure to fill this field.";
+      }),
 });
+
+export const fetchAddress = createAsyncThunk(
+  "user/fetchAddress",
+  async function () {
+    const positionObj = await getGeolocationPosition();
+    const position = {
+      latitude: positionObj.coords.latitude,
+      longitude: positionObj.coords.longitude,
+    };
+    const addressObj = await getAddress(position);
+    const address = `${addressObj?.locality}, ${addressObj?.city} ${addressObj?.postcode}, ${addressObj?.countryName} `;
+
+    // payload of the FULFILLED state
+    return { position, address };
+  }
+);
 
 export const { updateName } = userSlice.actions;
 
