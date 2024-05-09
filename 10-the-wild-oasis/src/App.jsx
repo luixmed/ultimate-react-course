8233ea7fc8b9f93cfc65c2a68fa3fa3378430398
Luis Medina <I202215560@cibertec.edu.pe>
2024-05-09 16:47:23 -0500

    feat: Create the routes foe each page of the project in the App component

--- 10-the-wild-oasis/src/App.jsx
+++ 10-the-wild-oasis/src/App.jsx
@@ -1,6 +1,12 @@
 import { GlobalStyle } from "./GlobalStyles";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
+import Bookings from "./pages/Bookings";
+import NewUsers from "./pages/Users";
+import Settings from "./pages/Settings";
+import Account from "./pages/Account";
+import Login from "./pages/Login";
+import PageNotFound from "./pages/PageNotFound";
 
 function App() {
   return (
@@ -9,6 +15,12 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="dashboard" element={<Dashboard />} />
+          <Route path="bookings" element={<Bookings />} />
+          <Route path="users" element={<NewUsers />} />
+          <Route path="settings" element={<Settings />} />
+          <Route path="account" element={<Account />} />
+          <Route path="login" element={<Login />} />
+          <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
     </>
