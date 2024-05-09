e84be2b1fc7c060fc4a8bd438622f86d621369df
Luis Medina <I202215560@cibertec.edu.pe>
2024-05-09 15:03:15 -0500

    feat: Create a reusable Input styled component

---
+++ 10-the-wild-oasis/src/ui/Input.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const Input = styled.input`
+  /* Box */
+  padding: 0.8rem 1.2rem;
+  border: 1px solid var(--color-grey-300);
+  border-radius: var(--border-radius-sm);
+
+  /* Style */
+  background-color: var(--color-grey-0);
+  box-shadow: var(--shadow-sm);
+`;
+
+export default Input;
