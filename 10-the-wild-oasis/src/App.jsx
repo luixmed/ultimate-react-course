c5007b3f9b5da9f3df6f66d37899661fb52adba8
Luis Medina <I202215560@cibertec.edu.pe>
2024-05-10 14:53:53 -0500

    fix: Change the staletime form 1 minute to 0 in the App component

--- 10-the-wild-oasis/src/App.jsx
+++ 10-the-wild-oasis/src/App.jsx
@@ -15,7 +15,8 @@ import Cabins from "./pages/Cabins";
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      staleTime: 60 * 1000,
+      // staleTime: 60 * 1000,
+      staleTime: 0,
     },
   },
 });
