043eadc86017e7063f0dc29436ab8e00b7483f75
Luis Medina <I202215560@cibertec.edu.pe>
2024-05-10 10:33:32 -0500

    feat: Add Supabase client initialization
    This commit adds a new file to initialize the Supabase client in the project. The file sets up the Supabase client using the createClient function from the "@supabase/supabase-js" library, providing the Supabase URL and API key. This initialization enables interaction with the Supabase backend services within the application.

---
+++ 10-the-wild-oasis/src/services/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = "https://umarsrbgllwxwfevonuf.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVtYXJzcmJnbGx3eHdmZXZvbnVmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDcxODEwMzEsImV4cCI6MjAyMjc1NzAzMX0.BFyJYlTM5KWeqpIxqxd7-LZlv922eA0KzwXX4pv5P0c";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
