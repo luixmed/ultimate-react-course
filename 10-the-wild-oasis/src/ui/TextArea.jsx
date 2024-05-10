b38b1f6c18452b5087c47b11c41624e0a5ff91ab
Luis Medina <I202215560@cibertec.edu.pe>
2024-05-10 16:24:55 -0500

    feat: Create a general reusable TextArea component

---
+++ 10-the-wild-oasis/src/ui/TextArea.jsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const Textarea = styled.textarea`
+  /* Box */
+  width: 100%;
+  height: 8rem;
+  padding: 0.8rem 1.2rem;
+  border: 1px solid var(--color-grey-300);
+  border-radius: 5px;
+
+  /* Style */
+  background-color: var(--color-grey-0);
+  box-shadow: var(--shadow-sm);
+`;
+
+export default Textarea;
