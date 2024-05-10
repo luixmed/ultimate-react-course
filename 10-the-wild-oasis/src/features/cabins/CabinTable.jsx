b735593ac7db23d04d2286e37915a12e7fb04461
Luis Medina <I202215560@cibertec.edu.pe>
2024-05-10 13:21:47 -0500

    feat: Implement CabinTable component for displaying cabin data
    This commit introduces the CabinTable component, responsible for rendering a table to display cabin information. It utilizes the data retrieved from the database to dynamically create a CabinRow component for each cabin. By mapping over the cabins data, we generate rows in the table to present detailed information about each cabin, enhancing the user experience.

--- 10-the-wild-oasis/src/features/cabins/CabinTable.jsx
+++ 10-the-wild-oasis/src/features/cabins/CabinTable.jsx
@@ -1,6 +1,8 @@
 import { useQuery } from "@tanstack/react-query";
 import styled from "styled-components";
 import { getCabins } from "../../services/apiCabins";
+import Spinner from "../../ui/Spinner";
+import CabinRow from "./CabinRow";
 
 const Table = styled.div`
   /* Box */
@@ -40,12 +42,30 @@ function CabinTable() {
   const {
     data: cabins,
     isLoading,
-    error,
+    // error,
   } = useQuery({
     queryKey: ["cabins"],
     queryFn: getCabins,
   });
-  return <div>TABLE</div>;
+
+  if (isLoading) return <Spinner />;
+
+  return (
+    <Table role="table">
+      <TableHeader role="row">
+        <div></div>
+        <div>Cabin</div>
+        <div>Capacity</div>
+        <div>Price</div>
+        <div>Discount</div>
+        <div></div>
+      </TableHeader>
+
+      {cabins.map((cabin) => (
+        <CabinRow cabin={cabin} key={cabin.id} />
+      ))}
+    </Table>
+  );
 }
 
 export default CabinTable;
