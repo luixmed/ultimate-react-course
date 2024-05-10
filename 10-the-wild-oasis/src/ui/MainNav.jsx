dac3c3141342e7c4b770a6577cf8f55eba29976c
Luis Medina <I202215560@cibertec.edu.pe>
2024-05-09 19:18:53 -0500

    feat: Create MainNav component
    This commit introduces the MainNav component, which includes links for the majority of pages within the application. By creating this component, we centralize navigation elements and streamline access to various pages, improving overall user navigation and experience.

---
+++ 10-the-wild-oasis/src/ui/MainNav.jsx
@@ -0,0 +1,70 @@
+import { NavLink } from "react-router-dom";
+import styled from "styled-components";
+
+const NavList = styled.ul`
+  display: flex;
+  flex-direction: column;
+  gap: 0.8rem;
+`;
+
+const StyledNavLink = styled(NavLink)`
+  &:link,
+  &:visited {
+    /* Font */
+    color: var(--color-grey-600);
+    font-size: 1.6rem;
+    font-weight: 500;
+
+    /* Box */
+    padding: 1.2rem 2.4rem;
+
+    /* Layout */
+    display: flex;
+    align-items: center;
+    gap: 1.2rem;
+
+    /* Animation */
+    transition: all 0.3s;
+  }
+
+  &:hover,
+  &:active,
+  &.active:link,
+  &.active:visited {
+    color: var(--color-grey-800);
+    border-radius: var(--border-radius-sm);
+    background-color: var(--color-grey-50);
+  }
+
+  & svg {
+    color: var(--color-grey-400);
+    width: 2.4rem;
+    height: 2.4rem;
+
+    transition: all 0.3s;
+  }
+
+  &:hover svg,
+  &:active svg,
+  &.active:link svg,
+  &.active:visited svg {
+    color: var(--color-brand-600);
+  }
+`;
+
+function MainNav() {
+  return (
+    <nav>
+      <NavList>
+        <li>
+          <StyledNavLink to="/dashboard">Dashboard</StyledNavLink>
+        </li>
+        <li>
+          <StyledNavLink to="/bookings">Bookings</StyledNavLink>
+        </li>
+      </NavList>
+    </nav>
+  );
+}
+
+export default MainNav;
