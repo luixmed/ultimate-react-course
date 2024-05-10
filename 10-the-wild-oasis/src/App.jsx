c651e3d2f962ec90137ee3ddf52d4774fed4fb07
Luis Medina <I202215560@cibertec.edu.pe>
2024-05-10 11:53:24 -0500

    feat: Integrate React Query for data fetching
    This commit configures React Query in the App component by creating a QueryClient instance with default options. The staleTime option is set to 60 seconds to control the cache behavior. The QueryClientProvider component wraps the application, providing access to the QueryClient instance throughout the component tree. Additionally, the ReactQueryDevtools component is included for debugging purposes, but initially hidden.

--- 10-the-wild-oasis/src/App.jsx
+++ 10-the-wild-oasis/src/App.jsx
@@ -1,5 +1,7 @@
 import { GlobalStyle } from "./GlobalStyles";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import Dashboard from "./pages/Dashboard";
 import Bookings from "./pages/Bookings";
 import NewUsers from "./pages/Users";
@@ -10,9 +12,18 @@ import AppLayout from "./ui/AppLayout";
 import Cabins from "./pages/Cabins";
 // import PageNotFound from "./pages/PageNotFound";
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 60 * 1000,
+    },
+  },
+});
+
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
       <GlobalStyle />
       <BrowserRouter>
         <Routes>
@@ -30,7 +41,7 @@ function App() {
           {/* <Route path="*" element={<PageNotFound />} /> */}
         </Routes>
       </BrowserRouter>
-    </>
+    </QueryClientProvider>
   );
 }
 
