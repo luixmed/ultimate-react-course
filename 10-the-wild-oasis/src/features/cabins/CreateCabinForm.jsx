962d5efd4f16f7e53f8ffacf9e0cd8eddf07285b
Luis Medina <I202215560@cibertec.edu.pe>
2024-05-10 16:30:53 -0500

    feat: Create basic structure for the CreateCabinForm component

---
+++ 10-the-wild-oasis/src/features/cabins/CreateCabinForm.jsx
@@ -0,0 +1,91 @@
+import styled from "styled-components";
+import Form from "../../ui/Form";
+import Input from "../../ui/Input";
+import Textarea from "../../ui/TextArea";
+import FileInput from "../../ui/FileInput";
+import Button from "../../ui/Button";
+
+const FormRow = styled.div`
+  /* Layout */
+  display: grid;
+  grid-template-columns: 24rem 1fr 1.2fr;
+  align-items: center;
+  gap: 2.4rem;
+
+  /* Box */
+  padding: 1.2rem 0;
+
+  &:first-child {
+    padding-top: 0;
+  }
+
+  &:last-child {
+    padding-bottom: 0;
+  }
+
+  &:not(:last-child) {
+    border-bottom: 1px solid var(--color-grey-100);
+  }
+
+  &:has(button) {
+    display: flex;
+    justify-content: flex-end;
+    gap: 1.2rem;
+  }
+`;
+
+const Label = styled.label`
+  font-weight: 500;
+`;
+
+// eslint-disable-next-line no-unused-vars
+const Error = styled.span`
+  color: var(--color-red-700);
+  font-size: 1.4rem;
+`;
+
+function CreateCabinForm() {
+  return (
+    <Form>
+      <FormRow>
+        <Label htmlFor="name">Cabin name</Label>
+        <Input type="text" id="name" />
+      </FormRow>
+
+      <FormRow>
+        <Label htmlFor="maxCapacity">Maximum capacity</Label>
+        <Input type="number" id="maxCapacity" />
+      </FormRow>
+
+      <FormRow>
+        <Label htmlFor="regularPrice">Regualr price</Label>
+        <Input type="number" id="regularPrice" />
+      </FormRow>
+
+      <FormRow>
+        <Label htmlFor="discount">Discount</Label>
+        <Input type="number" id="discount" defaultValue={0} />
+      </FormRow>
+
+      <FormRow>
+        <Label htmlFor="description">Description for website</Label>
+        <Textarea type="text" id="description" defaultValue="" />
+      </FormRow>
+
+      <FormRow>
+        <Label htmlFor="image">Cabin photo</Label>
+        <FileInput id="image" accept="image/*" />
+      </FormRow>
+
+      <FormRow>
+        {/* type is an HTML attribute! */}
+        <Button variation="secondary" type="reset">
+          Cancel
+        </Button>
+        <Button>Edit cabin</Button>
+      </FormRow>
+    </Form>
+  );
+}
+
+export default CreateCabinForm;
