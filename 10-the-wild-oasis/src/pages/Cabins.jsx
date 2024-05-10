febe780e363ecd5528ebe79b5e417ca4d6a77818
Luis Medina <I202215560@cibertec.edu.pe>
2024-05-10 12:30:05 -0500

    feat: Add the CabinTable component in the Cabin page

--- 10-the-wild-oasis/src/pages/Cabins.jsx
+++ 10-the-wild-oasis/src/pages/Cabins.jsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 import { getCabins } from "../services/apiCabins";
+import CabinTable from "../features/cabins/CabinTable";
 
 function Cabins() {
   useEffect(function () {
@@ -9,14 +10,16 @@ function Cabins() {
   }, []);
 
   return (
-    <Row type="horizontal">
-      <Heading as="h1">All cabins</Heading>
-      <p>TEST</p>
-      <img
-        src="https://umarsrbgllwxwfevonuf.supabase.co/storage/v1/object/public/cabin-images/cabin-001.jpg"
-        alt="cabin"
-      />
-    </Row>
+    <>
+      <Row type="horizontal">
+        <Heading as="h1">All cabins</Heading>
+        <p>Filter / Sort</p>
+      </Row>
+
+      <Row>
+        <CabinTable />
+      </Row>
+    </>
   );
 }
 
