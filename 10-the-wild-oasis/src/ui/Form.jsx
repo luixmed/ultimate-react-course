e9faf26b0d17e53636cce994d33cefb43c02c881
Luis Medina <I202215560@cibertec.edu.pe>
2024-05-10 16:11:45 -0500

    feat: Create a general reusable Form component

---
+++ 10-the-wild-oasis/src/ui/Form.jsx
@@ -0,0 +1,26 @@
+import styled, { css } from "styled-components";
+
+const Form = styled.form`
+  ${(props) =>
+    props.type !== "modal" &&
+    css`
+      /* Box */
+      padding: 2.4rem 4rem;
+      border: 1px solid var(--color-grey-100);
+      border-radius: var(--border-radius-md);
+
+      /* Style */
+      background-color: var(--color-grey-0);
+    `}
+
+  ${(props) =>
+    props.type === "modal" &&
+    css`
+      width: 80rem;
+    `}
+
+  overflow: hidden;
+  font-size: 1.4rem;
+`;
+
+export default Form;
