f81abc63f6be5609a29c51b4d2fc675381311034
Luis Medina <I202215560@cibertec.edu.pe>
2024-05-10 21:42:11 -0500

    feat: Introduce reusable FormRow component
    This commit adds the FormRow component, designed to encapsulate form input elements within a labeled row structure. The component consists of a Label component that receives its content as a prop and an input element passed as children. Additionally, the FormRow component displays any error message received as a prop, enhancing form validation and user feedback. By creating this reusable component, we improve code organization and maintainability while promoting consistent form layout and styling across the application.

---
+++ 10-the-wild-oasis/src/ui/FormRow.jsx
@@ -0,0 +1,51 @@
+import styled from "styled-components";
+
+const StyledFormRow = styled.div`
+  /* Layout */
+  display: grid;
+  grid-template-columns: 24rem 1fr 1.2fr;
+  align-items: center;
+  gap: 2.4rem;
+
+  /* Box */
+  padding: 1.2rem 0;
+
+  &:first-child {
+    padding-top: 0;
+  }
+
+  &:last-child {
+    padding-bottom: 0;
+  }
+
+  &:not(:last-child) {
+    border-bottom: 1px solid var(--color-grey-100);
+  }
+
+  &:has(button) {
+    display: flex;
+    justify-content: flex-end;
+    gap: 1.2rem;
+  }
+`;
+
+const Label = styled.label`
+  font-weight: 500;
+`;
+
+const Error = styled.span`
+  color: var(--color-red-700);
+  font-size: 1.4rem;
+`;
+
+function FormRow({ label, error, children }) {
+  return (
+    <StyledFormRow>
+      {label && <Label htmlFor={children.props.id}>{label}</Label>}
+      {children}
+      {error && <Error>{error}</Error>}
+    </StyledFormRow>
+  );
+}
+
+export default FormRow;
