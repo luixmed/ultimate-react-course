84fdae7c448dac895430519f8bc8fcf7bb35ae01
Luis Medina <I202215560@cibertec.edu.pe>
2024-05-11 17:07:14 -0500

    feat: Create the basic structure for the UpdateSettingForm

---
+++ 10-the-wild-oasis/src/features/settings/UpdateSettingForm.jsx
@@ -0,0 +1,27 @@
+import Form from "../../ui/Form";
+import FormRow from "../../ui/FormRow";
+import Input from "../../ui/Input";
+
+function UpdateSettingForm() {
+  return (
+    <Form>
+      <FormRow label="Minimum nights/booking">
+        <Input type="number" id="min-nights" />
+      </FormRow>
+
+      <FormRow label="Maximum nights/booking">
+        <Input type="number" id="max-nights" />
+      </FormRow>
+
+      <FormRow label="Maximum guests/booking">
+        <Input type="number" id="max-guests" />
+      </FormRow>
+
+      <FormRow label="Breakfast price">
+        <Input type="number" id="breakfast-price" />
+      </FormRow>
+    </Form>
+  );
+}
+
+export default UpdateSettingForm;
