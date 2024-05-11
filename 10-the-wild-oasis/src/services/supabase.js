5e43664af76f559a64b96b577193e8ef6342a38c
Luis Medina <I202215560@cibertec.edu.pe>
2024-05-10 22:38:42 -0500

    refactor: Make the supabaseUrl an export name variable
    This variables is the supabase url for our project

--- 10-the-wild-oasis/src/services/supabase.js
+++ 10-the-wild-oasis/src/services/supabase.js
@@ -1,5 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = "https://umarsrbgllwxwfevonuf.supabase.co";
+export const supabaseUrl = "https://umarsrbgllwxwfevonuf.supabase.co";
+
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVtYXJzcmJnbGx3eHdmZXZvbnVmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDcxODEwMzEsImV4cCI6MjAyMjc1NzAzMX0.BFyJYlTM5KWeqpIxqxd7-LZlv922eA0KzwXX4pv5P0c";
 const supabase = createClient(supabaseUrl, supabaseKey);
