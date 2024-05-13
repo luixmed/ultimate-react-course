28f7eee6f5fcfde216b82f20fc05672b4c90db6f
Luis Medina <I202215560@cibertec.edu.pe>
2024-05-13 17:54:41 -0500

    refactor: Use the Table component in the CabinTable component

--- 10-the-wild-oasis/src/features/cabins/CabinTable.jsx
+++ 10-the-wild-oasis/src/features/cabins/CabinTable.jsx
@@ -1,41 +1,7 @@
-import styled from "styled-components";
 import Spinner from "../../ui/Spinner";
 import CabinRow from "./CabinRow";
 import { useCabins } from "./useCabins";
-
-const Table = styled.div`
-  /* Box */
-  border: 1px solid var(--color-grey-200);
-  border-radius: 7px;
-
-  /* Font */
-  font-size: 1.4rem;
-
-  /* Style */
-  background-color: var(--color-grey-0);
-  overflow: hidden;
-`;
-
-const TableHeader = styled.header`
-  /* Layout */
-  display: grid;
-  grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
-  align-items: center;
-  column-gap: 2.4rem;
-
-  /* Style */
-  background-color: var(--color-grey-50);
-
-  /* Box */
-  padding: 1.6rem 2.4rem;
-  border-bottom: 1px solid var(--color-grey-100);
-
-  /* Font */
-  color: var(--color-grey-600);
-  font-weight: 600;
-  letter-spacing: 0.4px;
-  text-transform: uppercase;
-`;
+import Table from "../../ui/Table";
 
 function CabinTable() {
   const { isLoading, cabins } = useCabins();
@@ -43,15 +9,15 @@ function CabinTable() {
   if (isLoading) return <Spinner />;
 
   return (
-    <Table role="table">
-      <TableHeader role="row">
+    <Table columns="0.6fr 1.8fr 2.2fr 1fr 1fr 1fr">
+      <Table.Header>
         <div></div>
         <div>Cabin</div>
         <div>Capacity</div>
         <div>Price</div>
         <div>Discount</div>
         <div></div>
-      </TableHeader>
+      </Table.Header>
 
       {cabins.map((cabin) => (
         <CabinRow cabin={cabin} key={cabin.id} />
