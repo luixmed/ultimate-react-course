6727ae4d2e88c73a56ccc1b27cef626b646b9556
Luis Medina <I202215560@cibertec.edu.pe>
2024-05-13 11:02:50 -0500

    feat: Finish the first version of the AddCabin component

--- 10-the-wild-oasis/src/features/cabins/AddCabin.jsx
+++ 10-the-wild-oasis/src/features/cabins/AddCabin.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Button from "../../ui/Button";
 import Modal from "../../ui/Modal";
+import CreateCabinForm from "./CreateCabinForm";
 
 function AddCabin() {
   const [isOpenModal, setIsOpenModal] = useState(false);
@@ -10,7 +11,11 @@ function AddCabin() {
       <Button onClick={() => setIsOpenModal((show) => !show)}>
         Add new cabin
       </Button>
-      {isOpenModal && <Modal />}
+      {isOpenModal && (
+        <Modal onClose={() => setIsOpenModal(false)}>
+          <CreateCabinForm onCloseModal={() => setIsOpenModal(false)} />
+        </Modal>
+      )}
     </div>
   );
 }
