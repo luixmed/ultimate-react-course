0a39bc78f42c52ac0b317a7411b68d6f55f6570c
Luis Medina <I202215560@cibertec.edu.pe>
2024-05-13 14:52:58 -0500

    feat: Introduce ConfirmDelete component in CabinRow for cabin deletion confirmation
    Implement the ConfirmDelete component within the CabinRow to prompt users for confirmation before deleting a cabin. This enhancement enhances user experience by providing a safeguard against accidental deletions, improving usability and reducing the risk of data loss.

--- 10-the-wild-oasis/src/features/cabins/CabinRow.jsx
+++ 10-the-wild-oasis/src/features/cabins/CabinRow.jsx
@@ -5,6 +5,7 @@ import { useDeleteCabin } from "./useDeleteCabin";
 import { HiPencil, HiSquare2Stack, HiTrash } from "react-icons/hi2";
 import { useCreateCabin } from "./useCreateCabin";
 import Modal from "../../ui/Modal";
+import ConfirmDelete from "../../ui/ConfirmDelete";
 
 const TableRow = styled.div`
   /* Layout */
@@ -102,9 +103,20 @@ function CabinRow({ cabin }) {
           </Modal.Window>
         </Modal>
 
-        <button onClick={() => deleteCabin(cabinId)} disabled={isDeleting}>
-          <HiTrash />
-        </button>
+        <Modal>
+          <Modal.Open>
+            <button>
+              <HiTrash />
+            </button>
+          </Modal.Open>
+          <Modal.Window>
+            <ConfirmDelete
+              resourceName="cabins"
+              disabled={isDeleting}
+              onConfirm={() => deleteCabin(cabinId)}
+            />
+          </Modal.Window>
+        </Modal>
       </div>
     </TableRow>
   );
