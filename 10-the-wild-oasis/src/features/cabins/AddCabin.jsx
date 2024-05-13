f6250aade24022743c2b97a2b04ffc57767c2599
Luis Medina <I202215560@cibertec.edu.pe>
2024-05-13 13:34:57 -0500

    feat: Develop second version of AddCabin component
    Implement the second version of the AddCabin component utilizing the new Modal compound component. This version features dynamic rendering, displaying either the CreateCabinForm or the CabinTable based on user interaction. This enhancement improves modularity and user experience by providing a more versatile and intuitive interface for managing cabin data.

--- 10-the-wild-oasis/src/features/cabins/AddCabin.jsx
+++ 10-the-wild-oasis/src/features/cabins/AddCabin.jsx
@@ -1,16 +1,30 @@
 import Button from "../../ui/Button";
 import Modal from "../../ui/Modal";
+import CabinTable from "./CabinTable";
 import CreateCabinForm from "./CreateCabinForm";
 
 function AddCabin() {
-  <Modal>
-    <Modal.open opens="cabin-form">
-      <Button>Add new cabin</Button>
-    </Modal.open>
-    <Modal.Window name="cabin-form">
-      <CreateCabinForm />
-    </Modal.Window>
-  </Modal>;
+  return (
+    <>
+      <Modal>
+        <Modal.Open opens="cabin-form">
+          <Button>Add new cabin</Button>
+        </Modal.Open>
+        <Modal.Window name="cabin-form">
+          <CreateCabinForm />
+        </Modal.Window>
+      </Modal>
+
+      <Modal>
+        <Modal.Open opens="table">
+          <Button>Show table</Button>
+        </Modal.Open>
+        <Modal.Window name="table">
+          <CabinTable />
+        </Modal.Window>
+      </Modal>
+    </>
+  );
 }
 
 // FIRST VERSION
