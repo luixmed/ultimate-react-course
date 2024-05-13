d6243712dcb802f0db71e7af5d41865bf866e5d1
Luis Medina <I202215560@cibertec.edu.pe>
2024-05-13 15:01:23 -0500

    fix: Add missing props for the Modal component that displays the ConfirmDelete component in the CabinRow component

--- 10-the-wild-oasis/src/features/cabins/CabinRow.jsx
+++ 10-the-wild-oasis/src/features/cabins/CabinRow.jsx
@@ -104,12 +104,12 @@ function CabinRow({ cabin }) {
         </Modal>
 
         <Modal>
-          <Modal.Open>
+          <Modal.Open opens="delete">
             <button>
               <HiTrash />
             </button>
           </Modal.Open>
-          <Modal.Window>
+          <Modal.Window name="delete">
             <ConfirmDelete
               resourceName="cabins"
               disabled={isDeleting}
