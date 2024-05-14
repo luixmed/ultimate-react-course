9e89f5b638654efc54e190a60d39ba8e499d68ff
Luis Medina <I202215560@cibertec.edu.pe>
2024-05-14 13:11:34 -0500

    feat: Implement getBooking function to fetch a single booking by ID
    Introduce the getBooking function to retrieve a single booking from the Supabase database based on the provided ID. This function utilizes Supabase's client library to execute a select query on the 'bookings' table, including related data from 'cabins' and 'guest' tables using nested queries. Error handling is implemented to log any errors and throw an error message if the booking is not found.

--- 10-the-wild-oasis/src/services/apiBookings.js
+++ 10-the-wild-oasis/src/services/apiBookings.js
@@ -10,3 +10,18 @@ export async function getBookings() {
 
   return data;
 }
+
+export async function getBooking(id) {
+  const { data, error } = await supabase
+    .from("bookings")
+    .select("*, cabins(*), guest(*)")
+    .eq("id", id)
+    .single();
+
+  if (error) {
+    console.error(error);
+    throw new Error("Booking not found");
+  }
+
+  return data;
+}
