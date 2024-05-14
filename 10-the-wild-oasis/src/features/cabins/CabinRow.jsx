de4c4d04e9b77544e55312b5ec12c2adf501d721
Luis Medina <I202215560@cibertec.edu.pe>
2024-05-14 03:57:42 -0500

    feat: Integrate Menus component with cabin actions menu
    Utilize the Menus component to create a dropdown menu for cabin actions, including options for duplicating, editing, and deleting cabins. This enhancement improves user interaction by providing a convenient and visually consistent interface for performing common cabin-related tasks within the application.

--- 10-the-wild-oasis/src/features/cabins/CabinRow.jsx
+++ 10-the-wild-oasis/src/features/cabins/CabinRow.jsx
@@ -7,6 +7,7 @@ import { useCreateCabin } from "./useCreateCabin";
 import Modal from "../../ui/Modal";
 import ConfirmDelete from "../../ui/ConfirmDelete";
 import Table from "../../ui/Table";
+import Menus from "../../ui/Menus";
 
 const Img = styled.img`
   display: block;
@@ -103,6 +104,21 @@ function CabinRow({ cabin }) {
             />
           </Modal.Window>
         </Modal>
+
+        <Menus.Menu>
+          <Menus.Toggle id={cabinId} />
+
+          <Menus.List id={cabinId}>
+            <Menus.Button
+              icon={<HiSquare2Stack />}
+              onClick={handleDuplicateCabin}
+            >
+              Duplicate
+            </Menus.Button>
+            <Menus.Button icon={<HiPencil />}>Edit</Menus.Button>
+            <Menus.Button icon={<HiTrash />}>Delete</Menus.Button>
+          </Menus.List>
+        </Menus.Menu>
       </div>
     </Table.Row>
   );
