3e453a9fdefbd9d96c876df28a576cf0556dd369
Luis Medina <I202215560@cibertec.edu.pe>
2024-05-14 04:44:12 -0500

    feat: Introduce Filter component for horizontal button list
    Implement the Filter component, designed as a horizontal list of buttons. This component provides a convenient interface for selecting filter options, enhancing user experience and facilitating data filtering in the application.

---
+++ 10-the-wild-oasis/src/ui/Filter.jsx
@@ -0,0 +1,50 @@
+import styled from "styled-components";
+
+const StyledFilter = styled.div`
+  /* Box */
+  padding: 0.4rem;
+  border: 1px solid var(--color-grey-100);
+  border-radius: var(--border-radius-sm);
+
+  /* Layout */
+  display: flex;
+  gap: 0.4rem;
+
+  /* Style */
+  background-color: var(--color-grey-0);
+  box-shadow: var(--shadow-sm);
+`;
+
+const FilterButton = styled.button`
+  /* Font */
+  font-size: 1.4rem;
+  font-weight: 500;
+
+  /* Box */
+  padding: 0.44rem 0.8rem;
+  border: none;
+  border-radius: var(--border-radius-sm);
+
+  /* Style */
+  background-color: var(--color-grey-0);
+
+  /* Animation */
+  transition: all 0.3s;
+
+  &:hover:not(:disabled) {
+    color: var(--color-brand-50);
+    background-color: var(--color-brand-600);
+  }
+`;
+
+function Filter() {
+  return (
+    <StyledFilter>
+      <FilterButton>All</FilterButton>
+      <FilterButton>No discount</FilterButton>
+      <FilterButton>With discount</FilterButton>
+    </StyledFilter>
+  );
+}
+
+export default Filter;
