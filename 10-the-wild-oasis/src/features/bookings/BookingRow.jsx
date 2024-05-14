e81c0cff71a47319a0b6428e8556118a74dd7431
Luis Medina <I202215560@cibertec.edu.pe>
2024-05-14 15:16:55 -0500

    feat: Implement BookingRow component for displaying booking details
    Introduce the BookingRow component responsible for rendering individual booking details within the BookingTable. This component utilizes styled-components for styling and date-fns for date formatting and comparison. It displays relevant booking information such as cabin name, guest details, stay duration, status tag, and total price. The component enhances readability and presentation of booking data, contributing to an improved user experience.

--- 10-the-wild-oasis/src/features/bookings/BookingRow.jsx
+++ 10-the-wild-oasis/src/features/bookings/BookingRow.jsx
@@ -1,5 +1,85 @@
-function BookingRow() {
-  return <div>ROW</div>;
+import styled from "styled-components";
+import { format, isToday } from "date-fns";
+import Table from "../../ui/Table";
+import Tag from "../../ui/Tag";
+import { formatCurrency, formatDistanceFromNow } from "../../utils/helpers";
+
+const Cabin = styled.div`
+  /* Font */
+  font-family: "Sono";
+  color: var(--color-grey-600);
+  font-size: 1.6rem;
+  font-weight: 600;
+`;
+
+const Stacked = styled.div`
+  /* Layout */
+  display: flex;
+  flex-direction: column;
+  gap: 0.2rem;
+
+  & span:first-child {
+    font-weight: 500;
+  }
+
+  & span:last-child {
+    color: var(--color-grey-500);
+    font-size: 1.2rem;
+  }
+`;
+
+const Amount = styled.div`
+  font-family: "Sono";
+  font-weight: 500;
+`;
+
+function BookingRow({
+  booking: {
+    id: bookingId,
+    created_at,
+    startDate,
+    endDate,
+    numNights,
+    numGuests,
+    totalPrice,
+    status,
+    guests: { fullName: guestName, email },
+    cabins: { name: cabinName },
+  },
+}) {
+  const statusToTagName = {
+    unconfirmed: "blue",
+    "checked-in": "green",
+    "checked-out": "silver",
+  };
+
+  return (
+    <Table.Row>
+      <Cabin>{cabinName}</Cabin>
+
+      <Stacked>
+        <span>{guestName}</span>
+        <span>{email}</span>
+      </Stacked>
+
+      <Stacked>
+        <span>
+          {isToday(new Date(startDate))
+            ? "Today"
+            : formatDistanceFromNow(startDate)}
+          &rarr; {numNights} nights stay
+        </span>
+        <span>
+          {format(new Date(startDate), "MMM dd yyyy")} &mdash;
+          {format(new Date(endDate), "MMM dd yyyy")}
+        </span>
+      </Stacked>
+
+      <Tag type={statusToTagName[status]}>{status.replace("-", " ")}</Tag>
+
+      <Amount>{formatCurrency(totalPrice)}</Amount>
+    </Table.Row>
+  );
 }
 
 export default BookingRow;
