917c87da804a0b486f3dc21c9bbb09b30ee14aa2
Luis Medina <I202215560@cibertec.edu.pe>
2024-05-14 06:51:43 -0500

    feat: Enhance SortBy component with value prop for controlled selection
    Update the SortBy component to include a value prop, allowing for controlled selection of sorting options. This enhancement ensures that the selected sorting option reflects the current state stored in the URL query parameters, providing a seamless user experience when navigating between sorted views.

--- 10-the-wild-oasis/src/ui/SortBy.jsx
+++ 10-the-wild-oasis/src/ui/SortBy.jsx
@@ -3,12 +3,20 @@ import Select from "./Select";
 
 function SortBy({ options }) {
   const [searchParams, setSearchParams] = useSearchParams();
+  const sortByValue = searchParams.get("sortBy") || "";
 
   function handleChange(e) {
     searchParams.set("sortBy", e.target.value);
     setSearchParams(searchParams);
   }
-  return <Select options={options} type="white" onChange={handleChange} />;
+  return (
+    <Select
+      options={options}
+      value={sortByValue}
+      onChange={handleChange}
+      type="white"
+    />
+  );
 }
 
 export default SortBy;
