019af13ed279bdc90e5a906778b4e74c06887218
Luis Medina <I202215560@cibertec.edu.pe>
2024-05-14 04:46:10 -0500

    feat: Define TableOperations styled component
    Introduce TableOperations styled component, designed to contain operations or actions related to tables. This component utilizes styled-components for consistent styling and provides a flexible layout for aligning and spacing table-related operations, enhancing code reusability and maintainability.

---
+++ 10-the-wild-oasis/src/ui/TableOperations.jsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const TableOperations = styled.div`
+  /* Layout */
+  display: flex;
+  align-items: center;
+  gap: 1.6rem;
+`;
+
+export default TableOperations;
