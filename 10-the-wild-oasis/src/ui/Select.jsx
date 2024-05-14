88cfa01fd7b6eeafd3f735035606c86516b0ee70
Luis Medina <I202215560@cibertec.edu.pe>
2024-05-14 06:21:32 -0500

    feat: Implement styled Select component
    Introduce a styled Select component using styled-components, providing a customizable dropdown menu for selecting options. This component utilizes styled-components for consistent styling and supports dynamic rendering of options, enhancing flexibility and reusability for selection inputs across the application.

---
+++ 10-the-wild-oasis/src/ui/Select.jsx
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+
+const StyledSelect = styled.select`
+  /* Font */
+  font-size: 1.4rem;
+  font-weight: 500;
+
+  /* Box */
+  padding: 0.8rem 1.2rem;
+  border: 1px solid
+    ${(props) =>
+      props.type === "white"
+        ? "var(--color-grey-100)"
+        : "var(--color-grey-300)"};
+  border-radius: var(--border-radius-sm);
+
+  /* Style */
+  background-color: var(--color-grey-0);
+  box-shadow: var(--shadow-sm);
+`;
+
+function Select({ options }) {
+  return (
+    <StyledSelect>
+      {options.map((option) => (
+        <option value={option.value} key={option.value}>
+          {option.label}
+        </option>
+      ))}
+    </StyledSelect>
+  );
+}
+
+export default Select;
