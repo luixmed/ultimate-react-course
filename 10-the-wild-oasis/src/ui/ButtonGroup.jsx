89562f08f69de1c16ba299ba1c3bb6fc4226b071
Luis Medina <I202215560@cibertec.edu.pe>
2024-05-15 11:12:03 -0500

    feat: Create ButtonGroup styled component
    Created a styled component for a button group layout using flexbox for easy alignment and spacing of buttons.

---
+++ 10-the-wild-oasis/src/ui/ButtonGroup.jsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const ButtonGroup = styled.div`
+  /* Layout */
+  display: flex;
+  justify-content: flex-end;
+  gap: 1.2rem;
+`;
+
+export default ButtonGroup;
