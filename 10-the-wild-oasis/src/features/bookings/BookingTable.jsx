f8f42f75e2a709a90481765c640c7a62c65635c8
Luis Medina <I202215560@cibertec.edu.pe>
2024-05-14 20:41:05 -0500

    feat: Integrate Pagination component into BookingTable
    In this commit, the Pagination component has been integrated into the BookingTable component to enable pagination functionality for displaying bookings. The Pagination component is placed within the Table.Footer section to provide easy navigation between pages of bookings.

--- 10-the-wild-oasis/src/features/bookings/BookingTable.jsx
+++ 10-the-wild-oasis/src/features/bookings/BookingTable.jsx
@@ -1,5 +1,6 @@
 import Empty from "../../ui/Empty";
 import Menus from "../../ui/Menus";
+import Pagination from "../../ui/Pagination";
 import Spinner from "../../ui/Spinner";
 import Table from "../../ui/Table";
 import BookingRow from "./BookingRow";
@@ -29,6 +30,10 @@ function BookingTable() {
             <BookingRow key={booking.id} booking={booking} />
           )}
         />
+
+        <Table.Footer>
+          <Pagination count={45} />
+        </Table.Footer>
       </Table>
     </Menus>
   );
