ad7343f81f1518a0f43624dcc6a9aeebe960209a
Luis Medina <I202215560@cibertec.edu.pe>
2024-05-14 19:40:08 -0500

    feat: Enhance getBookings function to support sorting
    Modified the getBookings function to include sorting functionality. Added logic to apply sorting parameters to the query based on the sortBy object provided as an argument. This allows users to specify a field and direction for sorting the booking data retrieved from the database.

--- 10-the-wild-oasis/src/services/apiBookings.js
+++ 10-the-wild-oasis/src/services/apiBookings.js
@@ -1,6 +1,6 @@
 import supabase from "./supabase";
 
-export async function getBookings({ filter }) {
+export async function getBookings({ filter, sortBy }) {
   let query = supabase
     .from("bookings")
     .select(
@@ -8,8 +8,13 @@ export async function getBookings({ filter }) {
     );
 
   // FILTER
-  if (filter !== null)
-    query = query[filter.method || "eq"](filter.field, filter.value);
+  if (filter) query = query[filter.method || "eq"](filter.field, filter.value);
+
+  // SORT
+  if (sortBy)
+    query = query.order(sortBy.field, {
+      ascending: sortBy.direction === "asc",
+    });
 
   const { data, error } = await query;
 
