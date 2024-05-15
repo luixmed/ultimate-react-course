e3bca77ffa75124e939c9e008e6872d7073d42d8
Luis Medina <I202215560@cibertec.edu.pe>
2024-05-15 16:45:38 -0500

    feat: Create Checkbox component with styled layout and functionality
    This commit introduces the Checkbox component, designed to render a checkbox input with associated label text. The component utilizes styled-components to define its styling, ensuring consistent appearance across the application. The Checkbox component accepts props such as 'checked', 'onChange', 'disabled', and 'id' to customize its behavior and appearance. It enhances user interaction by providing a visually appealing and accessible checkbox input option.

---
+++ 10-the-wild-oasis/src/ui/Checkbox.jsx
@@ -0,0 +1,47 @@
+import styled from "styled-components";
+
+const StyledCheckbox = styled.div`
+  /* Layout */
+  display: flex;
+  gap: 1.6rem;
+
+  & input[type="checkbox"] {
+    /* Box */
+    height: 2.4rem;
+    width: 2.4rem;
+    outline-offset: 2px;
+
+    /* Style */
+    transform-origin: 0;
+    accent-color: var(--color-brand-600);
+  }
+
+  & input[type="checkbox"]:disabled {
+    accent-color: var(--color-brand-600);
+  }
+
+  & label {
+    flex: 1;
+
+    display: flex;
+    align-items: center;
+    gap: 0.8rem;
+  }
+`;
+
+function Checkbox({ checked, onChange, disabled = false, id, children }) {
+  return (
+    <StyledCheckbox>
+      <input
+        type="checkbox"
+        id={id}
+        checked={checked}
+        onChange={onChange}
+        disabled={disabled}
+      />
+      <label htmlFor={!disabled ? id : ""}>{children}</label>
+    </StyledCheckbox>
+  );
+}
+
+export default Checkbox;
