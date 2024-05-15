be42240548f0e7e6a8a87a6d1c63bcb07ebfc2f1
Luis Medina <I202215560@cibertec.edu.pe>
2024-05-15 11:26:34 -0500

    feat: Add navigation to BookingRow component
    Implemented navigation functionality in the BookingRow component to allow users to view booking details. This enhancement improves the user experience by providing easy access to detailed information about each booking.

--- 10-the-wild-oasis/src/features/bookings/BookingRow.jsx
+++ 10-the-wild-oasis/src/features/bookings/BookingRow.jsx
@@ -4,6 +4,9 @@ import { format, isToday } from "date-fns";
 import Table from "../../ui/Table";
 import Tag from "../../ui/Tag";
 import { formatCurrency, formatDistanceFromNow } from "../../utils/helpers";
+import Menus from "../../ui/Menus";
+import { HiEye } from "react-icons/hi2";
+import { useNavigate } from "react-router-dom";
 
 const Cabin = styled.div`
   /* Font */
@@ -48,6 +51,8 @@ function BookingRow({
     cabins: { name: cabinName },
   },
 }) {
+  const navigate = useNavigate();
+
   const statusToTagName = {
     unconfirmed: "blue",
     "checked-in": "green",
@@ -79,6 +84,18 @@ function BookingRow({
       <Tag type={statusToTagName[status]}>{status.replace("-", " ")}</Tag>
 
       <Amount>{formatCurrency(totalPrice)}</Amount>
+
+      <Menus.Menu>
+        <Menus.Toggle id={bookingId} />
+        <Menus.List id={bookingId}>
+          <Menus.Button
+            icon={<HiEye />}
+            onClick={() => navigate(`/bookings/${bookingId}`)}
+          >
+            See details
+          </Menus.Button>
+        </Menus.List>
+      </Menus.Menu>
     </Table.Row>
   );
 }
