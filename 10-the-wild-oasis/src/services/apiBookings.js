8fd653fc7c351f24f04a2adbe34ab97185440222
Luis Medina <I202215560@cibertec.edu.pe>
2024-05-15 07:57:17 -0500

    feat: Modify getBookings query to include count parameter
    This commit updates the getBookings function to include the count parameter in the query. The count parameter is set to "exact", ensuring the accurate retrieval of the total count of bookings along with the booking data. This enhancement improves the functionality by providing precise pagination information based on the total number of bookings.

--- 10-the-wild-oasis/src/services/apiBookings.js
+++ 10-the-wild-oasis/src/services/apiBookings.js
@@ -4,7 +4,8 @@ export async function getBookings({ filter, sortBy }) {
   let query = supabase
     .from("bookings")
     .select(
-      "id, created_at, startDate, endDate, numNights, numGuests, status, totalPrice, cabins(name), guests(fullName, email)"
+      "id, created_at, startDate, endDate, numNights, numGuests, status, totalPrice, cabins(name), guests(fullName, email)",
+      { count: "exact" }
     );
 
   // FILTER
@@ -16,14 +17,14 @@ export async function getBookings({ filter, sortBy }) {
       ascending: sortBy.direction === "asc",
     });
 
-  const { data, error } = await query;
+  const { data, error, count } = await query;
 
   if (error) {
     console.error(error.message);
     throw new Error("Bookings could not be loaded");
   }
 
-  return data;
+  return { data, count };
 }
 
 export async function getBooking(id) {
