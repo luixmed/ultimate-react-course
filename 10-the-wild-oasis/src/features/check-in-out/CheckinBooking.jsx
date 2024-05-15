0411764e29c472fab20f78ec0d31fa0713a9bfbc
Luis Medina <I202215560@cibertec.edu.pe>
2024-05-15 16:47:57 -0500

    feat: Enhance CheckinBooking component with confirmation checkbox and check-in button
    This commit extends the functionality of the CheckinBooking component by adding a confirmation checkbox and a check-in button. The confirmation checkbox allows users to confirm payment for the booking, dynamically updating its state. Additionally, the check-in button is enabled only when the payment confirmation is received, preventing accidental check-ins. This enhancement improves the user experience and ensures accurate processing of check-in requests.

--- 10-the-wild-oasis/src/features/check-in-out/CheckinBooking.jsx
+++ 10-the-wild-oasis/src/features/check-in-out/CheckinBooking.jsx
@@ -1,16 +1,37 @@
+import styled from "styled-components";
 import { useMoveBack } from "../../hooks/useMoveBack";
+import Button from "../../ui/Button";
+import ButtonGroup from "../../ui/ButtonGroup";
 import ButtonText from "../../ui/ButtonText";
 import Heading from "../../ui/Heading";
 import Row from "../../ui/Row";
 import Spinner from "../../ui/Spinner";
 import BookingDataBox from "../bookings/BookingDataBox";
 import { useBooking } from "../bookings/useBooking";
+import Checkbox from "../../ui/Checkbox";
+import { useEffect, useState } from "react";
+import { formatCurrency } from "../../utils/helpers";
+
+const Box = styled.div`
+  /* Box */
+  padding: 2.4rem 4rem;
+  border: 1px solid var(--color-grey-100);
+  border-radius: var(--border-radius-md);
+
+  /* Style */
+  background-color: var(--color-grey-0);
+`;
 
 function CheckinBooking() {
-  const { booking, isLoading } = useBooking();
   const moveBack = useMoveBack();
+  const { booking, isLoading } = useBooking();
+  const [confirmPaid, setConfirmPaid] = useState(false);
+
+  useEffect(() => setConfirmPaid(booking?.isPaid ?? false), [booking]);
 
   if (isLoading) return <Spinner />;
+
+  const { id: bookingId, guests, totalPrice } = booking;
 
   return (
     <>
@@ -20,6 +41,25 @@ function CheckinBooking() {
       </Row>
 
       <BookingDataBox booking={booking} />
+
+      <Box>
+        <Checkbox
+          checked={confirmPaid}
+          onChange={() => setConfirmPaid((confirm) => !confirm)}
+          id="confirm"
+          disabled={confirmPaid}
+        >
+          I confirm that {guests.fullName} has paid the total amount of{" "}
+          {formatCurrency(totalPrice)}
+        </Checkbox>
+      </Box>
+
+      <ButtonGroup>
+        <Button disabled={!confirmPaid}>Check in booking #{bookingId}</Button>
+        <Button variation="secondary" onClick={moveBack}>
+          Back
+        </Button>
+      </ButtonGroup>
     </>
   );
 }
