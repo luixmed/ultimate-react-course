4f69cb9c330c39a5ca88be387679fb1c5aefa71e
Luis Medina <I202215560@cibertec.edu.pe>
2024-05-15 08:46:38 -0500

    feat: Integrate pagination into BookingTable component
    This commit updates the BookingTable component to include the count received from the useBookings hook, enabling the Pagination component to display the correct total count of bookings. Now, the count prop passed to the Pagination component is dynamically set based on the count retrieved from the useBookings hook.

--- 10-the-wild-oasis/src/features/bookings/BookingTable.jsx
+++ 10-the-wild-oasis/src/features/bookings/BookingTable.jsx
@@ -7,7 +7,7 @@ import BookingRow from "./BookingRow";
 import { useBookings } from "./useBookings";
 
 function BookingTable() {
-  const { bookings, isLoading } = useBookings();
+  const { bookings, count, isLoading } = useBookings();
 
   if (isLoading) return <Spinner />;
   if (!bookings.length) return <Empty resource="bookings" />;
@@ -32,7 +32,7 @@ function BookingTable() {
         />
 
         <Table.Footer>
-          <Pagination count={45} />
+          <Pagination count={count} />
         </Table.Footer>
       </Table>
     </Menus>
