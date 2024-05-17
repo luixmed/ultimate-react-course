604e7d9207dbe69f817228beb74ee111be85e778
Luis Medina <I202215560@cibertec.edu.pe>
2024-05-16 21:59:46 -0500

    style: Add `h4` variant to Heading component
    This commit enhances the Heading component by introducing a new `h4` variant, which features a larger font size, increased font weight, and centered text alignment for improved heading hierarchy and emphasis.

--- 10-the-wild-oasis/src/ui/Heading.jsx
+++ 10-the-wild-oasis/src/ui/Heading.jsx
@@ -23,6 +23,14 @@ const Heading = styled.div`
       font-size: 2rem;
       font-weight: 500;
     `}
+
+  ${(props) =>
+    props.as === "h4" &&
+    css`
+      font-size: 3rem;
+      font-weight: 600;
+      text-align: center;
+    `}
 `;
 
 export default Heading;
